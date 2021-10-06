db4396fe396cd8c12f01b9d8547a36ff07a6964d
Salil Khanna <salilkhannawork@gmail.com>
2021-10-06 18:52:24 -0400

    first push for backend code

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e21b1a6310a1f091282cf0de841525b'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0e46aeb3-a174-4931-8abf-e28cdd48d1e9'
+};
