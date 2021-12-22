75fe6e697710f62e405b08c9c057e9e1731ed313
Kathy <kskathy123@yahoo.com>
2021-12-21 21:45:27 -0500

    landing page final

--- frontend/src/component/landingpage.jsx
+++ frontend/src/component/landingpage.jsx
@@ -1,227 +1,220 @@
 import React, { Component } from 'react'
-
+import Shape from '../component/TeamsShape.png'
+import Community from '../component/CommunitySection.JPG'
+import Blue from '../component/LearnMoreButton_Blue.png'
+import Yellow from '../component/LearnMoreButton_Yellow.png'
+import Student from '../component/StudentTriangle.png'
+import Client from '../component/ClientTriangle.png'
 
 
 export class landingpage extends Component {
   render() {
-    // const border= {
-    //   marginTop: 150,
-    //      border: '4px solid black', 
-    //      height: '750px',
-    //      backgroundColor: 'rgb(220,220,220)'
-    // }
-    // const border2 = {
-    //   marginTop: 80,
-    //      border: '4px solid black', 
-    //      height: '600px',
-    //      width: '500px',
-    //      marginRight: "100px",
-    //      marginLeft: "100px",
-    //      backgroundColor: 'rgb(220,220,220)'
-    // }
 
-    // const border3 = {
+     return (
+       <div>
+         {/* first section */}
+         <div className="GenerateText"> 
+           GENERATE
+         </div>
+         <div className="GenerateText2">
+           NORTHEASTERN'S PREMIER PRODUCT DEVELOPMENT LAB
+         </div>
+         <div className="margin">
+         <div class="row">
+         <div class="column">
+             <img style={{width:"80%", height:"800px"}} alt="GenerateImage1" src={"https://www.colorhexa.com/ffbf3c.png"}/>
+             <img style={{marginTop: "9%", width:"80%", height:"650px"}} alt="GenerateImage2" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
+
+             </div>
+             <div class="column">
+               <img style={{width:"140%", height:"1050px"}} alt="GenerateImage3" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
+               <div className="Generate">
+          
+               <div className="GenerateText4">
+               WHAT IS GENERATE?
+                 </div> 
+                 <div className="GenerateText3">
+                 With 186+ diverse engineers, entrepreneurs, designers and more, Generate offers a comprehensive Product Design service. We’ll get your vision from start to finish.
+               We support Northeastern University ventures. From hardware/software engineering to branding development, we satisfy all of their product development needs.
+               <div style={{paddingLeft: "65%", paddingTop: "5%"}}>
+                 
+                 <a href='http://localhost:3000/'><img alt="YellowButton" src={Yellow}/></a></div>
+               
+                 </div>
+                 
+               </div>
+              
+         
+             </div>
+             </div>
+         </div>
+
+         {/* divider */}
+         <div>
+           <img style={{width: "100%", height: "400px"}} alt="DividerImage1" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
+         </div>
+
+         {/* second section */}
+         <div>
+         <div className="ourTeam">
+
+           <div className="ourTeamText">OUR TEAMS</div>
+
+
+         <div class="wrapper" style={{margin:"5%"}}>
+  <div>
+    <div className="container">
+    <img style={{height:"338px"}} alt="TeamShape1" src={Shape}/>
+    <div className="overlay"></div>
+    </div>
+    <div className="TeamName">
+     HARDWARE
+    </div>
+    <br/>
+    <br/>
+    <div className="TeamDescription">
+      <p>Advantaging interdisciplinary engineering teams to meet out client's hardware needs</p>
+    </div>
+  </div>
+  <div>
+  <div className="container">
+    <img style={{height:"338px"}} alt="TeamShape2" src={Shape}/>
+    <div className="overlay"></div>
+    </div>
+  <div className="TeamName">
+      SOFTWARE
+    </div>
+    <br/>
+    <br/>
+    <div className="TeamDescription">
+      <p>Developing industry-leading software for client’s and Generate’s internal projects</p>
+    </div>
+  </div>
+  <div> <div className="container">
+    <img style={{height:"338px"}} alt="TeamShape3" src={Shape}/>
+    <div className="overlay"></div>
+    </div>
+  <div className="TeamName">
+      MANAGEMENT
+    </div>
+    <br/>
+    <br/>
+    <div className="TeamDescription">
+      <p>Leading Generate through collaboration and innovating thinking</p>
+    </div>
+    </div>
+  <div> <div className="container">
+    <img style={{height:"338px"}} alt="TeamShape4" src={Shape}/>
+    <div className="overlay"></div>
+    </div>
+  <div className="TeamName">
+      OUTREACH
+    </div>
+    <br/>
+    <br/>
+    <div className="TeamDescription">
+      <p>Fostering synergy between Generate’s clients and members</p>
+    </div>
+    </div>
+  <div> <div className="container">
+    <img style={{height:"338px"}} alt="TeamShape5" src={Shape}/>
+    <div className="overlay"></div>
+    </div>
+  <div className="TeamName">
+      OPERATIONS
+    </div>
+    <br/>
+    <br/>
+    <div className="TeamDescription">
+      <p>Optimizing Generate’s business functions</p>
+    </div>
+    </div>
+  <div> <div className="container">
+    <img style={{height:"338px"}} alt="TeamShape6" src={Shape}/>
+    <div className="overlay"></div>
+    </div>
+  <div className="TeamName">
+      COMMUNITY
+    </div>
+    <br/>
+    <br/>
+    <div className="TeamDescription">
+      <p>Pursuing unity for Generate’s clients and members</p>
+    </div>
+    </div>
+</div>
+     </div>
+
+
+           {/* divider */}
+         <div>
+           <img style={{width: "100%", height: "400px"}} alt="DividerImage2" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
+         </div>
+
+
+         {/* third section */}
+         <div className="ourCommunity">
       
-    //      border: '4px solid rgb(220,220,220)', 
-    //      height: '260px',
-    //      width: '625px',
-    //      marginLeft: "720px",
-    //      marginTop: "630px",
-    //      backgroundColor: 'rgb(220,220,220)'
-    // }
-    // const herotext = {
-    //      fontSize: 90,
-    //      fontWeight: '700',
-    //      textAlign: "left",
-    //      paddingLeft: 200,
-    //      paddingTop: 150,
-    // };
+           <div className="ourCommunityText">OUR COMMUNITY</div>
+           <img style={{float:"right", width: "50%", height: "1006px", marginTop: "115px", marginRight: "150px"}} alt="CommunityImage1" src={Community}/>
+           <div style={{position: "absolute",}}className="ourCommunityWhite">
+             <div  className="font" style={{ textAlign:"left", fontSize: "40px", padding: "3%", lineHeight: "59px"}}>
+             Enabling and growing an inclusive, supportive, and lighthearted community that cultivates entrepreneurs and 
+             leaders in Generate.
+             </div>            
+           </div>
 
-    const ourteam = {
-      fontSize: 90,
-      fontWeight: '700',
-      paddingTop: 50,
-      textAlign: 'center',
- };
-   
- const dummy = {
-  fontSize: 40,
-  fontWeight: '650',
-  textAlign: "left",
-  paddingLeft: '13%',
-  paddingRight: '23%',
-  paddingTop: 50,
-};
-
-const row = {
-  display: 'inline-block'
-}
-
-const column = {
-float:'left',
-margin:'40px',
-}
-
-const student = {
-  border: '4px solid blue', 
-  backgroundColor: 'blue',
-  height: '750px',
-  width: '10%',
-  float: 'left'
-}
-
-const client = {
-  border: '4px solid orange', 
-  backgroundColor: 'orange',
-  height: '750px',
-  width: '10%',
-  float: 'right'
-}
+           <div className="ourCommunityText2">
+           Our community is a strong central pillar of Generate’s culture, with a whole team dedicated to planning engaging semesterly events and activities for members including: 
+           retreats, our ‘all-hands’ meeting, and our end of semester showcase. We are constantly looking to grow inclusivity within our organization, with long term projects 
+           including re-evaluating our hiring process, and building resources for current and future leaders.
+           <div style={{paddingTop: "10%"}}>
+             <a href='http://localhost:3000/'>
+             <img alt="BlueButton" src={Blue}></img></a>
+             </div>
+           </div>
+           
+           
+         </div>
 
 
+            {/* divider */}
+            <div>
+           <img style={{width: "100%", height: "400px"}} alt="DividerImage3" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
+         </div>
+
+{/* fourth section */}
+         <div>
+           <div className="font" style={{textAlign: "center", fontSize: "40px", lineHeight: "60px"}}>
+           Join a collaborative family of diverse <br/> engineers, inquisitive designers, <br/> savvy entrepreneurs and more!
+           </div>
+
+           <div className="applyText"> I <br/>AM<br/> A</div>
 
 
+           <div className="student">
+             <img alt="StudentImage" src={Student}/>
+           </div>
 
-    return (
-      <div>
-      <div className="border">
-        <img
-                  style={{
-                  "width":"44%",
-                  "height":"1000px",
-                  "border":"4px solid black",
-                  "position": "absolute",
-                  "marginTop": 100,}}
-                  src={""} alt="image1"/>
-        <div className="herotext">Hero Text</div>
-
-        <div style = {dummy}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-        Aenean commodo ligula eget dolor. Aenean massa.</div>
-        </div>
+           <div className="client">
+             <img alt="ClientImage" src={Client}/>
+           </div>
         
-      
         
+         {/* <div className="student">
+           <span style={{marginTop: "150%", writingMode: "vertical-rl",
+           transform: "rotate(180deg)", fontSize:"108px", fontWeight:"bold"}}>STUDENT</span>
+           </div>
+           <div className="client">
+             <span style={{marginTop: "200%", writingMode: "vertical-rl", fontSize:"108px", fontWeight:"bold"}}>CLIENT
+             </span>
+             </div> */}
+        
+         </div>
 
-        <div className="border2">
-        <img  src={""} alt="image2"/></div>
+         </div>
 
-        <div className="border3" style={{"textAlign": 'left'}}>
-        <div style={{"fontSize": 50, "fontWeight": 700}}>What is Generate?</div>
-        <br></br>
-        <div style={{"fontSize": 20, "paddingRight": "300px"}}>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus 
-        et magnis dis parturient montes, nascetur ridiculus mus. Aenean commodo ligula eget dolor. </div>
-        <br></br> 
-       <a href="#" style={{"fontSize": 25, "color": "black", "fontWeight": 700, "textDecoration": "none"}}>LINK TO ABOUT US  </a>
-        </div>
-
-
-
-<div className="border4">
-
-<div style = {ourteam}>Our Teams</div>
-
-<div class={`container`}>
-    <div style = {row}>
-        <div style = {column}>
-        <div class={`container`}>
-          <div style={{textAlign: "left"}}>
-        <img  style={{width: "340px", height: "292px"}}src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="} alt="teamimage1"/>
-        <div style={{"fontSize": 60, "fontWeight": 700}}>Team1</div>
-        Lorem ipsum dolor sit amet, <br></br> consectetuer adipiscing elit. <br></br>
-        <a href="#" style={{"fontSize": 23, "color": "black", "textDecoration": "none"}}>LINK TO ABOUT US </a>
-        </div>
-        </div>
-        </div>
-        <div style = {column}>
-        <div style={{textAlign: "left"}}>
-        <img  style={{width: "340px", height: "292px"}}src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="} alt="teamimage2"/>
-        <div style={{"fontSize": 60, "fontWeight": 700}}>Team2</div>
-        Lorem ipsum dolor sit amet, <br></br> consectetuer adipiscing elit. <br></br>
-        <a href="#" style={{"fontSize": 23, "color": "black", "textDecoration": "none"}}>LINK TO ABOUT US </a>
-        </div>
-        </div>
-        <div style = {column}>
-        <div style={{textAlign: "left"}}>
-        <img  style={{width: "340px", height: "292px"}}src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="} alt="teamimage3"/>
-        <div style={{"fontSize": 60, "fontWeight": 700}}>Team3</div>
-        Lorem ipsum dolor sit amet, <br></br> consectetuer adipiscing elit. <br></br>
-        <a href="#" style={{"fontSize": 23, "color": "black", "textDecoration": "none"}}>LINK TO ABOUT US </a>
-        </div>
-        </div>
-    </div>
-    <div ></div>
-    <div style = {row}>
-        <div style = {column}>
-        <div style={{textAlign: "left"}}>
-        <img  style={{width: "340px", height: "292px"}}src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="} alt="teamimage4"/>
-        <div style={{"fontSize": 60, "fontWeight": 700}}>Team4</div>
-        Lorem ipsum dolor sit amet, <br></br> consectetuer adipiscing elit. <br></br>
-        <a href="#" style={{"fontSize": 23, "color": "black", "textDecoration": "none"}}>LINK TO ABOUT US </a>
-        </div>
-        </div>
-        <div style = {column}>
-        <div style={{textAlign: "left"}}>
-        <img  style={{width: "340px", height: "292px"}}src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="}  alt="teamimage5"/>
-        <div style={{"fontSize": 60, "fontWeight": 700}}>Team5</div>
-        Lorem ipsum dolor sit amet, <br></br> consectetuer adipiscing elit. <br></br>
-        <a href="#" style={{"fontSize": 23, "color": "black", "textDecoration": "none"}}>LINK TO ABOUT US </a>
-        </div>
-        </div>
-        <div style = {column}>
-        <div style={{textAlign: "left"}}>
-        <img  style={{width: "340px", height: "292px"}}src={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARMAAAC3CAMAAAAGjUrGAAAAA1BMVEX///+nxBvIAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBgxUwAAU+n3sIAAAAASUVORK5CYII="}  alt="teamimage6"/>
-        <div style={{"fontSize": 60, "fontWeight": 700}}>Team6</div>
-        Lorem ipsum dolor sit amet, <br></br> consectetuer adipiscing elit. <br></br>
-        <a href="#" style={{"fontSize": 23, "color": "black", "textDecoration": "none"}}>LINK TO ABOUT US </a>
-        </div>
-        </div>
-    </div>
-    </div>
-</div>
-
-
-
-<div>
-<div style = {{'paddingTop':'200px', 'fontSize':'180px', 'fontWeight': 'bold', 'color': 'gray'}}>
-COMMUNITY
-</div>
-
-<img
-                  style={{
-                  "width":"45%",
-                  "height":"1000px",
-                  "border":"4px solid black",
-                  "position": "absolute",
-                  "backgroundColor":  'rgb(220,220,220)',
-                  "marginTop": 70,
-                  "marginLeft": -90,
-                  "border-radius": '50px 0px 0px 50px'}}
-                  src={""} alt="image3"/>
-<div style={{'marginTop':'590px',
-'marginRight':'850px',
-'marginLeft':'130px',
-'textAlign':'left',
-'fontSize': '25px'}}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus 
-        et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus 
-        et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-        Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus 
-        et magnis dis parturient montes, nascetur ridiculus mus.
-        </div>
-</div>
-
-<div style={{'paddingTop':'400px'}}>
-<div style={student}><span style={{writingMode: "vertical-rl",
-  transform: "rotate(180deg)", fontSize:"120px", fontWeight:"bold"}}>student</span></div>
-<div style={client}><span style={{writingMode: "vertical-rl", fontSize:"120px", fontWeight:"bold", color:"white"}}>client</span></div>
-
-  <div style={{'fontSize':'180px', 'fontWeight': 'bold',}}> I <br/>Am<br/> A</div>
-  
-  
-</div>
+         
         </div>
         
     );
