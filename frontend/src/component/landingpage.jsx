97a4f7e91d20d1b6a54107a0c3dcac7a49f1c36c
Kathy <kskathy123@yahoo.com>
2021-12-22 20:15:51 -0500

    last landing page push

--- frontend/src/component/landingpage.jsx
+++ frontend/src/component/landingpage.jsx
@@ -13,27 +13,27 @@ export class landingpage extends Component {
      return (
        <div>
          {/* first section */}
-         <div className="GenerateText"> 
+         <div className="ldp_GenerateText"> 
            GENERATE
          </div>
-         <div className="GenerateText2">
+         <div className="ldp_GenerateText2">
            NORTHEASTERN'S PREMIER PRODUCT DEVELOPMENT LAB
          </div>
-         <div className="margin">
-         <div class="row">
-         <div class="column">
+         <div className="ldp_margin">
+         <div class="ldp_row">
+         <div class="ldp_column">
              <img style={{width:"80%", height:"800px"}} alt="GenerateImage1" src={"https://www.colorhexa.com/ffbf3c.png"}/>
              <img style={{marginTop: "9%", width:"80%", height:"650px"}} alt="GenerateImage2" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
 
              </div>
-             <div class="column">
+             <div class="ldp_column">
                <img style={{width:"140%", height:"1050px"}} alt="GenerateImage3" src={"https://i.redd.it/0tbg92b1ziu41.jpg"}/>
-               <div className="Generate">
+               <div className="ldp_Generate">
           
-               <div className="GenerateText4">
+               <div className="ldp_GenerateText4">
                WHAT IS GENERATE?
                  </div> 
-                 <div className="GenerateText3">
+                 <div className="ldp_GenerateText3">
                  With 186+ diverse engineers, entrepreneurs, designers and more, Generate offers a comprehensive Product Design service. We’ll get your vision from start to finish.
                We support Northeastern University ventures. From hardware/software engineering to branding development, we satisfy all of their product development needs.
                <div style={{paddingLeft: "65%", paddingTop: "5%"}}>
@@ -56,89 +56,89 @@ export class landingpage extends Component {
 
          {/* second section */}
          <div>
-         <div className="ourTeam">
+         <div className="ldp_ourTeam">
 
-           <div className="ourTeamText">OUR TEAMS</div>
+           <div className="ldp_ourTeamText">OUR TEAMS</div>
 
 
-         <div class="wrapper" style={{margin:"5%"}}>
+         <div class="ldp_wrapper" style={{margin:"5%"}}>
   <div>
-    <div className="container">
+    <div className="ldp_container">
     <img style={{height:"338px"}} alt="TeamShape1" src={Shape}/>
-    <div className="overlay"></div>
+    <div className="ldp_overlay"></div>
     </div>
-    <div className="TeamName">
+    <div className="ldp_TeamName">
      HARDWARE
     </div>
     <br/>
     <br/>
-    <div className="TeamDescription">
+    <div className="ldp_TeamDescription">
       <p>Advantaging interdisciplinary engineering teams to meet out client's hardware needs</p>
     </div>
   </div>
   <div>
-  <div className="container">
+  <div className="ldp_container">
     <img style={{height:"338px"}} alt="TeamShape2" src={Shape}/>
-    <div className="overlay"></div>
+    <div className="ldp_overlay"></div>
     </div>
-  <div className="TeamName">
+  <div className="ldp_TeamName">
       SOFTWARE
     </div>
     <br/>
     <br/>
-    <div className="TeamDescription">
+    <div className="ldp_TeamDescription">
       <p>Developing industry-leading software for client’s and Generate’s internal projects</p>
     </div>
   </div>
-  <div> <div className="container">
+  <div> <div className="ldp_container">
     <img style={{height:"338px"}} alt="TeamShape3" src={Shape}/>
-    <div className="overlay"></div>
+    <div className="ldp_overlay"></div>
     </div>
-  <div className="TeamName">
+  <div className="ldp_TeamName">
       MANAGEMENT
     </div>
     <br/>
     <br/>
-    <div className="TeamDescription">
+    <div className="ldp_TeamDescription">
       <p>Leading Generate through collaboration and innovating thinking</p>
     </div>
     </div>
-  <div> <div className="container">
+  <div> <div className="ldp_container">
     <img style={{height:"338px"}} alt="TeamShape4" src={Shape}/>
-    <div className="overlay"></div>
+    <div className="ldp_overlay"></div>
     </div>
-  <div className="TeamName">
+  <div className="ldp_TeamName">
       OUTREACH
     </div>
     <br/>
     <br/>
-    <div className="TeamDescription">
+    <div className="ldp_TeamDescription">
       <p>Fostering synergy between Generate’s clients and members</p>
     </div>
     </div>
-  <div> <div className="container">
+  <div> <div className="ldp_container">
     <img style={{height:"338px"}} alt="TeamShape5" src={Shape}/>
     <div className="overlay"></div>
     </div>
-  <div className="TeamName">
+  <div className="ldp_TeamName">
       OPERATIONS
     </div>
     <br/>
     <br/>
-    <div className="TeamDescription">
+    <div className="ldp_TeamDescription">
       <p>Optimizing Generate’s business functions</p>
     </div>
     </div>
-  <div> <div className="container">
+  <div> <div className="ldp_container">
     <img style={{height:"338px"}} alt="TeamShape6" src={Shape}/>
-    <div className="overlay"></div>
+    <div className="ldp_overlay"></div>
     </div>
-  <div className="TeamName">
+  <div className="ldp_TeamName">
       COMMUNITY
     </div>
     <br/>
     <br/>
-    <div className="TeamDescription">
+    <div className="ldp_TeamDescription">
       <p>Pursuing unity for Generate’s clients and members</p>
     </div>
     </div>
@@ -153,18 +153,18 @@ export class landingpage extends Component {
 
 
          {/* third section */}
-         <div className="ourCommunity">
+         <div className="ldp_ourCommunity">
       
-           <div className="ourCommunityText">OUR COMMUNITY</div>
+           <div className="ldp_ourCommunityText">OUR COMMUNITY</div>
            <img style={{float:"right", width: "50%", height: "1006px", marginTop: "115px", marginRight: "150px"}} alt="CommunityImage1" src={Community}/>
-           <div style={{position: "absolute",}}className="ourCommunityWhite">
-             <div  className="font" style={{ textAlign:"left", fontSize: "40px", padding: "3%", lineHeight: "59px"}}>
+           <div style={{position: "absolute",}}className="ldp_ourCommunityWhite">
+             <div  className="ldp_font" style={{ textAlign:"left", fontSize: "40px", padding: "3%", lineHeight: "59px"}}>
              Enabling and growing an inclusive, supportive, and lighthearted community that cultivates entrepreneurs and 
              leaders in Generate.
              </div>            
            </div>
 
-           <div className="ourCommunityText2">
+           <div className="ldp_ourCommunityText2">
            Our community is a strong central pillar of Generate’s culture, with a whole team dedicated to planning engaging semesterly events and activities for members including: 
            retreats, our ‘all-hands’ meeting, and our end of semester showcase. We are constantly looking to grow inclusivity within our organization, with long term projects 
            including re-evaluating our hiring process, and building resources for current and future leaders.
@@ -185,30 +185,23 @@ export class landingpage extends Component {
 
 {/* fourth section */}
          <div>
-           <div className="font" style={{textAlign: "center", fontSize: "40px", lineHeight: "60px"}}>
+           <div className="ldp_font" style={{textAlign: "center", fontSize: "40px", lineHeight: "60px"}}>
            Join a collaborative family of diverse <br/> engineers, inquisitive designers, <br/> savvy entrepreneurs and more!
            </div>
 
-           <div className="applyText"> I <br/>AM<br/> A</div>
+           <div className="ldp_applyText"> I <br/>AM<br/> A</div>
 
 
-           <div className="student">
+           <div className="ldp_student">
              <img alt="StudentImage" src={Student}/>
            </div>
 
-           <div className="client">
+           <div className="ldp_client">
              <img alt="ClientImage" src={Client}/>
            </div>
         
         
-         {/* <div className="student">
-           <span style={{marginTop: "150%", writingMode: "vertical-rl",
-           transform: "rotate(180deg)", fontSize:"108px", fontWeight:"bold"}}>STUDENT</span>
-           </div>
-           <div className="client">
-             <span style={{marginTop: "200%", writingMode: "vertical-rl", fontSize:"108px", fontWeight:"bold"}}>CLIENT
-             </span>
-             </div> */}
+        
         
          </div>
 
