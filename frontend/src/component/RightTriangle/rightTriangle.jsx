954ffc9214bd2e223d7def436ee1363f7024b55a
amymin00 <amy.min7117@gmail.com>
2022-02-25 19:55:45 -0500

    forgot to add right triangle component directory oops

---
+++ frontend/src/component/RightTriangle/rightTriangle.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './rightTriangle.css';
+
+export default function RightTriangle(props) {
+    const propsBackgroundColor = props.bgColor;
+
+    return (
+        <div className={`same-w-h position-absolute ${props.className}`}
+             style={{height: props.height,
+                    background: `linear-gradient(to ${props.orientation}, 
+                                 rgba(0, 0, 0, 0) 0%, 
+                                 rgba(0, 0, 0, 0) 50%, 
+                                 ${propsBackgroundColor} 50%, 
+                                 ${propsBackgroundColor} 100%)`,
+                    }}>
+        </div>
+    );
+}
