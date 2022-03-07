a572b4abfa468052190d5c10e6029c09a32ebf29
Kathy <kskathy123@yahoo.com>
2022-03-07 17:04:07 -0500

    apply page new changes

--- frontend/src/component/ApplyPage/applyPageMain.jsx
+++ frontend/src/component/ApplyPage/applyPageMain.jsx
@@ -4,10 +4,56 @@ import React, { Component } from 'react'
 export class applyPageMain extends Component {
     render() {
         return (
-            <div id="apb_applyPage">
-                <h1>
-                    Apply Page
-                </h1>
+            <div>   
+
+
+<div className="ap_container">
+  <div className="ap_img"></div>
+  <div className="ap_text">WE VIBE HARD AND</div>
+  <div className="ap_text">MAKE THINGS</div>
+  <div className="ap_text2">
+  We’re always looking for passionate individuals who are ready to learn and grow. 
+  We have opportunities that run broad and deep. If you don’t see anything right now drop an email and we’ll let you know when posiitons open up again.
+  </div>
+
+  <div className="ap_link1">
+      COMMUNITY
+  </div>
+
+  <div className="ap_link1">
+      CONTENT CREATION
+  </div>
+
+  <div className="ap_link1">
+      DATA SCIENCE
+  </div>
+
+  <div className="ap_link1">
+      DESIGN
+  </div>
+
+  <div className="ap_link1">
+      EVENT PLANNING
+  </div>
+
+  <div className="ap_link1">
+      HARDWARE ENGINEERING
+  </div>
+
+  <div className="ap_link1">
+      LEADERSHIP
+  </div>
+
+  <div className="ap_link1">
+      OPERATIONS
+  </div>
+
+  <div className="ap_link1">
+      SOFTWARE ENGINEERING
+  </div>
+</div>
+
+
             </div>
         );
     }
