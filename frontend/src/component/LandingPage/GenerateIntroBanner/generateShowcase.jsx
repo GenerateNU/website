2f24906137c7a68543700c1b86cb0498e3375fe3
amymin00 <amy.min7117@gmail.com>
2022-03-22 15:34:23 -0400

    added some media queries for intro. banner

--- frontend/src/component/LandingPage/GenerateIntroBanner/generateShowcase.jsx
+++ frontend/src/component/LandingPage/GenerateIntroBanner/generateShowcase.jsx
@@ -6,24 +6,24 @@ import RightTriangle from '../../RightTriangle';
 import visitShowcase from '../../assets/images/Visit_Showcase.jpg';
 import './style.css';
 
-// ...
+// TODO: Abstract with TheSherm component
 export class GenerateShowcase extends Component {
     render() {        
         return (
-            <Row className='m-0 px-5 align-items-center justify-content-center'>
-                <Col xs={12} sm={6} className='position-relative p-0 pe-5'>
+            <Row className='h-100 m-0 px-md-5 align-items-center justify-content-center'>
+                <Col xs={12} sm={6} className='h-100 p-0 pe-4 ps-md-0 ps-2'>
                     <a href="">
                         <h1 className='text-uppercase subheader-style pb-4'
                             style={{'text-decoration-color': '#FFC539'}}>
                             Visit Our Showcase
                         </h1>
                     </a>
-                    <p className='subheader-info-style'>
+                    <p className='subheader-info-style subheader-info-size'>
                         Take a look at our showcase videos from previous semesters to see some of Generate's past projects!
                     </p>
                 </Col>
 
-                <Col xs={12} sm={6} className='p-0 ps-4 h-100 position-relative overflow-hidden justify-content-center'>
+                <Col xs={12} sm={6} className='h-100 p-0 ps-md-4 position-relative overflow-hidden justify-content-center'>
                     <div className='h-100 position-relative'>
                         <Image src={visitShowcase} className='fit-image' />
                         <RightTriangle height='25%' color='white' orientation='top left' className='top-0 start-0' />
