1ef1256bd25a31f85ce0c741f038019234f0ced8
eraricamehra <mehra.e@northeastern.edu>
2022-03-23 14:02:18 -0400

    Added js files to get all positions

---
+++ frontend/src/component/ApplyPage/positions.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Positions extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            positions :[]
+        }
+    }
+
+   async componentWillMount() {
+      await  fetch('http://localhost:1337/api/positions').then((response) => {
+            if(response.status >= 400) {
+                throw new Error('Bad Response')
+            }
+            return response.json();
+        }).then((data) => {
+            // console.log(data)
+            // data.data.forEach(i => 
+            //     console.log(i.attributes.PositionTitle, "positions"))
+            this.setState({positions: data.data});
+        })
+
+    }
+
+    render() {
+        return this.state.positions.length > 0 && (
+            <div>
+                {
+                    this.state.positions.map((position) => {
+                        return(<div>
+                            <h1>{position && position.attributes && position.attributes.PositionTitle}</h1>
+                        </div>)
+                    })
+                }
+            </div>
+        )
+
+    }
+}
+export default Positions;
