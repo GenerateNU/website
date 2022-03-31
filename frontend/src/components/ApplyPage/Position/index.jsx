a122681cc9bcf398b8fff0973b1f6d994c7bd063
amymin00 <amy.min7117@gmail.com>
2022-03-31 16:05:35 -0400

    changed position fields, abstracted useFetch

--- frontend/src/components/ApplyPage/Position/index.jsx
+++ frontend/src/components/ApplyPage/Position/index.jsx
@@ -1,38 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import useFetch from '../../useFetch';
 import ShadowedButton from '../ShadowedButton';
 import './style.css';
 import { useParams } from 'react-router-dom';
-
-const useFetch = url => {
-    const [data, setData] = useState(null);
-
-    const fetchPosition = async url => {
-        console.log(url, "url")
-        //change api call
-        await  fetch(url).then((response) => {
-            if(response.status >= 400) {
-                throw new Error('Bad Response')
-            }
-            return response.json();
-        }).then(data => {
-            //console.log(data, "fetchPositions")
-            //let positionTitles = data.data.filter(i => i.attributes.CategoryType === currentCategory);
-            // let positionTitles = data.data;
-            // console.log(positionTitles, "xxxx"); 
-            setData(data);
-            // console.log(this.state.positions, "positions")
-        });
-    }
-
-    useEffect(() => {
-        fetchPosition(url)
-    }, [url]);
-
-    return data;
-}
 
 export default function Position() {
     const { id } = useParams();
@@ -40,12 +13,8 @@ export default function Position() {
     const data = useFetch(`http://localhost:1337/api/positions/${id}`);
 
     if (data === null) {
-        return <div>:( didn't work</div>
-    } 
-    // else {
-    //     console.log(data);
-    //     return JSON.stringify(data, null, '\t');
-    // }
+        return <div>Error fetching data from server</div>
+    }
 
     const position = data.data.attributes;
 
@@ -139,27 +108,24 @@ export default function Position() {
                         <Col className='date'>
                             <h5>Duration</h5>
                             {/* NOTE: I think this should be separated into start and end months fields */}
-                            <h3>{position.duration}</h3>
-                            {/* May 2022 - May 2023 */}
+                            <h4>{position.startDate} - {position.endDate}</h4>
                         </Col>
                         <Col className='hours'>
                             <h5>Weekly commitment</h5>
                             {/* NOTE: Field does not yet exist */}
-                            {/* <h3>{position.weeklyCommitment}</h3> */}
-                            <h4>Up to (number) hours</h4>
+                            <h4>Up to {position.weeklyCommitment} hours</h4>
                         </Col>
                     </Row>
                     <h4 className='pt-4 pb-5'>
                         {/* NOTE: Field does not yet exist */}
                         This position begins and ends with the Northeastern academic
-                        semester. The last day to apply is (date).
-                        {/* {position.applicationDeadline}. */}
+                        semester. The last day to apply is {position.applicationDeadline}.
                     </h4>
 
                     {/* Link to information about position type */}
                     <a className='text-decoration-none' href='#'>
                         {/* TODO: extract management, keyword probably diff. per position type */}
-                        <u> learn more about the (team name) team</u> -- &gt;
+                        <u> learn more about the <span className='text-lowercase'>{position.categoryType}</span> team</u> -- &gt;
                     </a>
                 </Col>
             </Row>
