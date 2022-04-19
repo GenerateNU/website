282ac446b1d5070e2d8f6e5f68ea155565178e01
Mayan Mishra <mishra.ma@northeastern.edu>
2022-04-19 12:41:44 -0400

    History page

--- frontend/src/component/AboutPage/history.jsx
+++ frontend/src/component/AboutPage/history.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import ShermImg from '../AboutPage/NUShermanCenter.png'
+import MosaicImg from '../AboutPage/MosaicLogo.png'
 
 export default function history() {
   return (
     <div>
-      hi
+      <div id="container_main">
+        <div className="left_panel">
+          <div className="leftPanel_container">
+            <div className="dir">
+              <p>PARENT</p>
+              <p>ORGS</p>
+            </div>
+
+            <div className="Orgs">
+              <div className="Sherm">
+                <p>The Sherm</p>
+                <img src={ShermImg} alt="" />
+              </div>
+              <div className="Mosaic">
+                <p>Mosaic</p>
+                <img src={MosaicImg} alt="" height="25%" width="25%"/>
+              </div>
+              <p></p>
+            </div>
+          </div>
+        </div>
+        <div className="right_panel">
+          <div className="rightPanel_container">
+            <div className="rightPanel_Head">
+              <p>OUR</p>
+              <p>HISTORY</p>
+            </div>
+
+            <div className="rightPanel_text">
+              <p>
+                There was a hole in Northeastern University’s entrepreneurial
+                ecosystem, and a lack of opportunities for student ventures to
+                elicit support from product builders. To fix that, Generate was
+                created to bridge the gap for students, and our journey began.
+              </p>
+
+              <h1>&gt;</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
