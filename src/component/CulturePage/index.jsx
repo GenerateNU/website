b6fcebab951acd4df313fe9d7ccefc44c66a1274
Kathy <kskathy123@yahoo.com>
2022-09-14 20:44:45 -0400

    culture branch

--- src/component/CulturePage/index.jsx
+++ src/component/CulturePage/index.jsx
@@ -3,9 +3,10 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import './style.css';
 
-export default class AboutPage extends React.Component {
+export default class CulturePage extends React.Component {
     render() {
         return (
+
             <Container fluid className='vh-100 horizontal-scroll'>
                 <Row className='flex-nowrap'>
          {/* Add components of culture page here  */}
