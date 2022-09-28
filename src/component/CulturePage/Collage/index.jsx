ef1379545df878a6ad5b7878f25f1df4bda2ebc5
Kathy <kskathy123@yahoo.com>
2022-09-27 23:32:25 -0400

    collage format added

--- src/component/CulturePage/Collage/index.jsx
+++ src/component/CulturePage/Collage/index.jsx
@@ -12,24 +12,74 @@ export default function collage() {
   </div>
   <div class="item item2">2</div>
   <div class="item item3">3</div>
-  <div class="item item4">4</div>
-  <div class="item item5">5</div>
+  <div class="item item4">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item5">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
   <div class="item item6">6</div>
-  <div class="item item7">7</div>
-  <div class="item item8">8</div>
-  <div class="item item9">9</div>
-  <div class="item item10">10</div>
-  <div class="item item11">11</div>
-  <div class="item item12">12</div>
-  <div class="item item13">13</div>
-  <div class="item item14">14</div>
-  <div class="item item15">15</div>
-  <div class="item item16">16</div>
-  <div class="item item17">17</div>
-  <div class="item item18">18</div>
-  <div class="item item19">19</div>
-  <div class="item item20">20</div>
-  <div class="item item21">21</div>
+  <div class="item item7">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item8">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img></div>
+  <div class="item item9">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item10">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item11">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item12">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item13">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item14">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item15">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item16">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item17">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item18">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item19">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item20">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
+  <div class="item item21">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png">
+</img>
+  </div>
 </div>
 
     </div>
