b729bbd48a9f931550cc0beaf4e0d489f775bf93
eraricamehra <mehra.e@northeastern.edu>
2022-09-29 21:16:03 -0400

    Pushing Landing Page V2 branch

---
+++ src/component/LandingPageV2/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import './style.css';
+
+export default class AboutPage extends React.Component {
+    render() {
+        return (
+            <Container fluid className='vh-100 horizontal-scroll bg-black'>
+                <Row className='flex-nowrap'>
+                   
+                </Row>
+            </Container>
+        );
+    }
+}
