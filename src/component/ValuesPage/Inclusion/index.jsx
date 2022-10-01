1b077c6f9dddf5115aab90a025672ac114928db0
Henry DeGrasse <henrydegrasse@gmail.com>
2022-09-30 23:11:57 -0400

    almost done kinda

--- src/component/ValuesPage/Inclusion/index.jsx
+++ src/component/ValuesPage/Inclusion/index.jsx
@@ -7,13 +7,13 @@ export default class Diversity extends React.Component {
           <div className="diversity">
             <div className="sideways">
             <h1>
-                <span class="white-text">DIVERSITY</span>
-                <span class="green-text">DIVERSITY</span>
-                <span class="white-text">DIVERSITY</span>
+                <span class="white-text">Inclusion</span>
+                <span class="purple-text">Inclusion</span>
+                <span class="white-text">Inclusion</span>
             </h1>
             </div>
             <div className="number">
-                <h2>1.0</h2>
+                <h2>2.0</h2>
             </div>
             <div className="descriptions">
             <p>
@@ -21,6 +21,7 @@ export default class Diversity extends React.Component {
                 nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec elit
                 tempus risus. Tincidunt sagittis nulla orci id. Augue risus
                 adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
+                Tincidunt sagittis nulla orci id. Augue risus adipiscing mi eleifend.
             </p>
             </div>
           </div>
--- src/component/ValuesPage/index.jsx
+++ src/component/ValuesPage/index.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
-import Diversity from "./Diversity";
 import './style.css';
+import Diversity from "./Diversity";
 
 export default class ValuesPage extends React.Component {
     render() {
         return (
-
-            <Container fluid className="vh-100 horizontal-scroll bg-light">
-        <Row className="flex-nowrap">
-          <Diversity />
-        </Row>
-      </Container>
+            <Container fluid className='vh-100 horizontal-scroll bg-white'>
+                <Row className='flex-nowrap'>
+                    <Diversity />
+                </Row>
+            </Container>
         );
     }
 }
