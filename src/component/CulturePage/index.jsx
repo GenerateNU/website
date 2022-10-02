12c22a905575b59fdd5c6760c219c457be3ed53a
eraricamehra <mehra.e@northeastern.edu>
2022-10-02 19:00:40 -0400

    Fixed conflict

--- src/component/CulturePage/index.jsx
+++ src/component/CulturePage/index.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import "./style.css";
-import IntroSection from "./IntroSection";
-import IntroImages from "./IntroImages";
-import BelongHere from "./BelongHere";
 
 export default class CulturePage extends React.Component {
   render() {
     return (
       <Container fluid className="vh-100 horizontal-scroll bg-light">
         <Row className="flex-nowrap">
-          <IntroSection />
-          <IntroImages />
-          <BelongHere />
+           {/* Add components of culture page here  */}
+        
         </Row>
       </Container>
     );
