d7479e797d0e9bb2f5d668713a73635c0a8e95ea
eraricamehra <mehra.e@northeastern.edu>
2022-10-05 21:43:43 -0400

    Added flex properties

--- src/component/AboutPage/GrowthMantra/index.jsx
+++ src/component/AboutPage/GrowthMantra/index.jsx
@@ -5,19 +5,33 @@ const GrowthMantra = () => {
     return (
         // <div className="position-relative vw-100 vh-100 py-4 px-5">
           <div className="background-white">
-            <div className="Growth-text">Growth</div>
-            <div className="IsOurText">Is Our</div>
-            <div className="MantraText">Mantra</div>
-            <div className="paragraphText">Our values arent just surface level but run deep throughout our organization. We always encourage our members to take on new challenges. Tools and resources are only an ask away, and members can test out their ideas to drive development.</div>
+            <div className="alignItemsRight">
+            <div className="mantra">
+            <p className="Growth-text">Growth</p>
+            <p className="IsOurText">Is Our</p>
+            <p className="MantraText">Mantra</p>
+            </div>
+            {/* TODO */}
+            <div>ghsegjgsjg</div>
+            </div>
+            
+            {/* <div className="paragraphText">Our values arent just surface level but run deep throughout our organization. We always encourage our members to take on new challenges. Tools and resources are only an ask away, and members can test out their ideas to drive development.</div> */}
           <div className="alignItemsRight">
             {/* <div className= "paragraphTextTwo">Teams work side-by-side to ensure products and processes are beyond expectations. We are self-driven and help one another reach goals and become leaders. Not only do we build products, but we also build community, leadership, and opportunities.</div> */}
-            <div className= "paragraphTextTwo">hdsgkhgk</div>
-          </div>
+            <div className= "paragraphText">Our values arent just surface level but run deep throughout our organization. We always encourage our members to take on new challenges. Tools and resources are only an ask away, and members can test out their ideas to drive development.</div>
          
-            <div className= "interestedText">Interested</div>
-            <div className= "applyNowBox">
+          
+            {/* <div className= "paragraphTextTwo">Teams work side-by-side to ensure products and processes are beyond expectations. We are self-driven and help one another reach goals and become leaders. Not only do we build products, but we also build community, leadership, and opportunities.</div> */}
+            <div className= "paragraphText">Our values arent just surface level but run deep throughout our organization. We always encourage our members to take on new challenges. Tools and resources are only an ask away, and members can test out their ideas to drive development.</div>
+            {/* TODO */}
+            <div>Apply button</div>
+            
+         </div>
+         
+            
+            {/* <div className= "applyNowBox">
               <div className="applyNowText">apply now</div>
-            </div>
+            </div> */}
         </div>
     );
 };
