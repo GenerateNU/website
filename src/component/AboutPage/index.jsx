719f77d636cc9831355e7c1f5fd4b2adca7240b2
Preeti Makaram <makaram.pr@northeastern.edu>
2022-10-05 20:23:03 -0400

    Fixed compilation error

--- src/component/AboutPage/index.jsx
+++ src/component/AboutPage/index.jsx
@@ -5,36 +5,32 @@ import OurMissionSection from "./OurMissionSection";
 import WeAreSection from "./WeAreSection";
 import Leadership from "./Leadership";
 import History from "./History";
-import AboutHistory from './AboutHistory'
-import Timeline from './Timeline'
+import AboutHistory from "./AboutHistory";
+import Timeline from "./Timeline";
 import WhyGenerateCarousel from "./WhyGenerateCarousel";
 import Footer from "../VerticalFooter";
 import FooterTwo from "../VerticalFooterTeams";
-import GrowthMantra from "./GrowthMantra";
-import './style.css';
+import "./style.css";
 
 export default class AboutPage extends React.Component {
-    render() {
-        return (
-            <Container fluid className='vh-100 horizontal-scroll bg-black'>
-                <Row className='flex-nowrap'>
-                    
-                    <OurMissionSection />
-                    <WeAreSection />
-                    <Leadership />
-                   
-                    {/* <AboutHistory /> */}
-                    <History />
-                    <Timeline />
-                    {/* <Members/> */}
+  render() {
+    return (
+      <Container fluid className="vh-100 horizontal-scroll bg-black">
+        <Row className="flex-nowrap">
+          <OurMissionSection />
+          <WeAreSection />
+          <Leadership />
 
-                    <WhyGenerateCarousel />
-                    <GrowthMantra/>
-                    <Footer/>
-                    <FooterTwo/>
-                   
-                </Row>
-            </Container>
-        );
-    }
+          {/* <AboutHistory /> */}
+          <History />
+          <Timeline />
+          {/* <Members/> */}
+
+          <WhyGenerateCarousel />
+          <Footer />
+          <FooterTwo />
+        </Row>
+      </Container>
+    );
+  }
 }
