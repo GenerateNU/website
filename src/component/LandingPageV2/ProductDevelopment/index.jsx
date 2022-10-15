85f7154a5e32cc2210808c6ab9792b1ba067c49a
Vinay-Balaji <55262835+Vinay-Balaji@users.noreply.github.com>
2022-10-15 17:45:36 -0400

    testing

--- src/component/LandingPageV2/ProductDevelopment/index.jsx
+++ src/component/LandingPageV2/ProductDevelopment/index.jsx
@@ -6,13 +6,14 @@ const ProductDevelopment = () => {
     return (
         // <div className="position-relative vw-100 vh-100 py-4 px-5">
           <div className="background-black">
-            <div classname = "alignRight">
+            <div classname = "mantra">
+              <div className="ourProcessText">our process </div>
+              <div className="ProductDevText">Product Development Cycle </div>
+              <div className= "paragraphText">Generates product development cycle is a roadmap to guide any products lifetime. We base our process on common industry practice, using an iterative process that includes research, prototyping and production. Though each products journey is unique and often challenging, our reliable framework can bring your vision to life. </div>
+            </div>
+            <div classname = "alignItemsRight">
               <img src ={DevelopmentWheel} />
             </div>
-            <div className="ourProcessText">our process </div>
-            <div className="ProductDevText">Product Development Cycle </div>
-            <div className= "paragraphText">Generates product development cycle is a roadmap to guide any products lifetime. We base our process on common industry practice, using an iterative process that includes research, prototyping and production. Though each products journey is unique and often challenging, our reliable framework can bring your vision to life. </div>
-            
         </div>
     );
 };
