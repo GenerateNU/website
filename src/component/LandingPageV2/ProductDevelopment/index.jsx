871d0d67a74af0228bb2dbfa937ac8f74f84bd9e
Vinay-Balaji <55262835+Vinay-Balaji@users.noreply.github.com>
2022-10-17 20:48:14 -0400

    Changes

--- src/component/LandingPageV2/ProductDevelopment/index.jsx
+++ src/component/LandingPageV2/ProductDevelopment/index.jsx
@@ -15,6 +15,40 @@ const ProductDevelopment = () => {
             <div className = "DevelopmentWheel">
               <img src ={DevelopmentWheel} />
             </div>
+
+
+            <div className = "DevelopmentWheel">
+              <img src ={DevelopmentWheel} />
+            </div>
+            <div className="ideation">
+              <div className = "column">
+                <div className="IdeationText">Ideation</div>
+                <div className= "paragraphText">Ideation involves the synthesis, communication, and validation of novel concepts as they relate to a new product. We start with problem identification to define the scope of the problem and begin market research to begin thinking about a solution. The last steps of ideation is for our teams to plan out ideas until a final concept is decided,  expanding upon the previous research and digging deeper into the pros, cons, and implementation of a given technology. </div>
+              </div>
+            </div>
+
+            <div className = "DevelopmentWheel">
+              <img src ={DevelopmentWheel} />
+            </div>
+            <div className="ideation">
+              <div className = "column">
+                <div className="PrototypeText">Prototype</div>
+                <div className= "paragraphText">Research and concept generation during the Ideation phase is synthesized into a test product. The prototyping phase is unique in that it’s split into 2 tracks: Experience and Functionality. Each track of the prototyping phase is  done iteratively. An experienced prototype model will be designed based on how the final product should look, feel and be interacted with to undergo testing. The functionality track will focus on how the product should work and be tested under those conditions. </div>
+              </div>
+            </div>
+
+            <div className = "DevelopmentWheel">
+              <img src ={DevelopmentWheel} />
+            </div>
+            <div className="ideation">
+              <div className = "column">
+                <div className="ProductionText">Production</div>
+                <div className= "paragraphText">During the production phase, the Experience and Functionality prototypes  are merged into a single design (the Engineering Product), which is then validated in an increasingly rigorous way to ensure that the product can be manufactured.  Once those steps have been completed, then the product is ready to be produced and sold, ending our product development process with satisfied clients. </div>
+              </div>
+            </div>
+
+
+
             </div>  
         </div>
     );
