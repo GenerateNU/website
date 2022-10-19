42d601bcfd042a322fc39a74b498fac8c971b5da
eraricamehra <mehra.e@northeastern.edu>
2022-10-19 18:51:48 -0400

    Added spacing

--- src/component/CulturePage/BelongHere/index.jsx
+++ src/component/CulturePage/BelongHere/index.jsx
@@ -13,22 +13,27 @@ export default function BelongHere() {
         
         <div className="belong-here">
           <h2 className="heading-text">Belong Here</h2>
-          <p className="belong-here-text">
+          <div className="culture-text-box">
+          <p className="culture-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
             nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec elit
             tempus risus. Tincidunt sagittis nulla orci id. Augue risus
             adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
           </p>
+
+          </div>
         </div>
         <div className="thrive-together">
          
           <h2 className="heading-text">Thrive Together</h2>
-          <p>
+          <div className="culture-text-box">
+          <p className="culture-text">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
             nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec elit
             tempus risus. Tincidunt sagittis nulla orci id. Augue risus
             adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
           </p>
+          </div>
         </div>
       </div>
       </div>
--- src/component/CulturePage/Equity/index.jsx
+++ src/component/CulturePage/Equity/index.jsx
@@ -12,14 +12,15 @@ export default function Equity() {
 
         <div className="text-div">
             <h2>1.0</h2>
-       
-        
-                <p>
+            <div className="culture-text-box">
+             <p className="culture-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
                     nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec elit
                     tempus risus. Tincidunt sagittis nulla orci id. Augue risus
                     adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
                 </p>
+
+            </div>
        
         </div>
       </div>
