cfe6187ef6ae24be4a5b1216c0b46d6df1383b31
Kathy <kskathy123@yahoo.com>
2022-10-19 19:46:04 -0400

    menu updated

--- src/component/NavBar/index.js
+++ src/component/NavBar/index.js
@@ -72,11 +72,11 @@ const NavBar = () => {
                                 </a>
                             </Row>
                             <Row className='m-0 p-0'>
-                                <a className='header-font-style fit-content text-decoration-none text-black'
-                                    href="/"
+                                <div className='disabled-font-style fit-content text-decoration-none text-black'
+                                    
                                 >
                                     Projects
-                                </a>
+                                </div>
                             </Row>
                         </Col>
                         <Col xs={2} className='m-0 p-0 pt-2'>
