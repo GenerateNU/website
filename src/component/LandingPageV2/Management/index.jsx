bc38bc9bf675c1f5a3bb1bcb51c984342577bfec
Kathy <kskathy123@yahoo.com>
2022-10-19 21:07:25 -0400

    first wheel

---
+++ src/component/LandingPageV2/Management/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './style.css';
+
+export default function Management() {
+    // const renderTooltip = (type, props) => (
+    //     <Tooltip id="button-tooltip" 
+    //             {...props}>
+    //         <p className='mb-0 p-2 text-dark tooltip-text'>
+    //             {
+    //                 (
+    //                     type === 'sherman' &&
+    //                     <>
+    //                         <span className='fw-bold'>The Sherman Center</span> is a program that enables Northeastern student entrepreneurship by providing education, tools and resources. The program is designed to arm engineering students with proper entrepreneurial skills to pitch and commercialize their ideas.
+    //                     </>
+    //                 ) ||
+    //                 <>
+    //                     <span className='fw-bold'>Mosaic</span> is a group of entrepreneurially-spirited people who share a passion for building new products.They allow the community to share experiences and exchange ideas Mosaic programs distinguish Northeastern University students as leaders in entrepreneurship and innovation.
+    //                 </>
+    //             }
+    //         </p>
+    //     </Tooltip>
+    // );
+
+    return (
+       <div>
+       
+       <nav class="circle-menu">
+  <div class="center"></div>
+  <ul>
+    <li><a href="#">monitor</a></li>
+    <li><a href="#">aperture</a></li>
+    <li><a href="#">audio</a></li>
+    <li><a href="#">battery</a></li>
+    <li><a href="#">bluetooth</a></li>
+    <li><a href="#">browser</a></li>
+  </ul>
+</nav>
+
+
+
+
+        </div>
+    );
+}
--- src/component/LandingPageV2/index.jsx
+++ src/component/LandingPageV2/index.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import './style.css';
+import Management from "./Management"
 
 export default class AboutPage extends React.Component {
     render() {
         return (
-            <Container fluid className='vh-100 horizontal-scroll bg-black'>
+            <Container fluid className='vh-100 horizontal-scroll'>
                 <Row className='flex-nowrap'>
+                    
+                    <Management />
+                  
                    
                 </Row>
             </Container>
