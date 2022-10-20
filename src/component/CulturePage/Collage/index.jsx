f579cf85eca9ee662e844d6622b9a1129008404a
eraricamehra <mehra.e@northeastern.edu>
2022-10-19 21:20:52 -0400

    Fixed collage

--- src/component/CulturePage/Collage/index.jsx
+++ src/component/CulturePage/Collage/index.jsx
@@ -5,53 +5,53 @@ export default function collage() {
   return (
     <div>
 <div class="grid">
-  <div class="item item1">
+  <div class="itemx item1">
 <img src="https://images.unsplash.com/photo-1530104091755-015d31dfa0b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1418&q=80">
 </img>
   </div>
-  <div class="item item2"></div>
-  <div class="item item3"></div>
-  <div class="item item4">
+  <div class="itemx item2"></div>
+  <div class="itemx item3"></div>
+  <div class="itemx item4">
   <img src="https://images.unsplash.com/photo-1529859719044-0c1f60408097?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img>
   </div>
-  <div class="item item5">
+  <div class="itemx item5">
   <img src="https://images.unsplash.com/photo-1529156362931-fa144f6d01eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1622&q=80">
 </img>
   </div>
-  <div class="item item6"></div>
-  <div class="item item7">
+  <div class="itemx item6"></div>
+  <div class="itemx item7">
   <img src="https://images.unsplash.com/photo-1510739859545-e7b9e979de86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80">
 </img>
   </div>
-  <div class="item item8">
+  <div class="itemx item8">
     <img src="https://images.unsplash.com/photo-1538739204988-d0b7904aba5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80">
 </img></div>
-  <div class="item item9">
+  <div class="itemx item9">
   <img src="https://images.unsplash.com/photo-1610222178399-6874ab11f6d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80">
 </img>
   </div>
-  <div class="item item10">
+  <div class="itemx item10">
   <img src="https://images.unsplash.com/photo-1567866885012-3c7171d2c5ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80">
 </img>
   </div>
-  <div class="item item11">
+  <div class="itemx item11">
   <img src="https://images.unsplash.com/photo-1563136183-f073e60c7033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img>
   </div>
-  <div class="item item12">
+  <div class="itemx item12">
   <img src="https://images.unsplash.com/photo-1506529086606-b82a5b9e9219?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img>
   </div>
-  <div class="item item13">
+  <div class="itemx item13">
   <img src="https://images.unsplash.com/photo-1538404291012-dfa9e233b73c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img>
   </div>
-  <div class="item item14">
+  <div class="itemx item14">
   <img src="https://images.unsplash.com/photo-1503891455988-54b97c8dfc4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img>
   </div>
-  <div class="item item15">
+  <div class="itemx item15">
   <img src="https://images.unsplash.com/photo-1503587209101-a62878726384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img>
   </div>
