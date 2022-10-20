a15cff7c44d64030c825a9693b935116f2788b6f
Preeti Makaram <makaram.pr@northeastern.edu>
2022-10-19 21:00:42 -0400

    styling for die components

--- src/component/CulturePage/Inclusion/index.jsx
+++ src/component/CulturePage/Inclusion/index.jsx
@@ -3,25 +3,26 @@ import "./style.css";
 
 export default function Inclusion() {
   return (
-      <div className="container">
-        <div className="vertical-div">
-            <span className="vertical">Inclusion</span>
-            <span className="vertical">Inclusion</span>
-            <span className="vertical">Inclusion</span>
-        </div>
+    <div className="container">
+      <div className="vertical-div">
+        <span className="outline-inclusion">uity</span>
+        <span style={{ color: "#A559EC" }} className="vertical">
+          Equity
+        </span>
+        <span className="outline-inclusion">Equ</span>
+      </div>
 
-        <div className="text-div">
-            <h2>1.0</h2>
-            <div className="culture-text-box">
-             <p className="culture-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
-                    nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec elit
-                    tempus risus. Tincidunt sagittis nulla orci id. Augue risus
-                    adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
-                </p>
-
-            </div>
+      <div className="text-div-inclusion">
+        <h2>2.0</h2>
+        <div className="culture-text-box">
+          <p className="culture-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
+            nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec elit
+            tempus risus. Tincidunt sagittis nulla orci id. Augue risus
+            adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
+          </p>
         </div>
       </div>
+    </div>
   );
 }
