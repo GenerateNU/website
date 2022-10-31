423d10b2c804a08026bf7a51ca3c2e9919baadce
Kathy <kskathy123@yahoo.com>
2022-10-31 19:57:05 -0400

    title added

--- src/component/LandingPageV2/Management/index.jsx
+++ src/component/LandingPageV2/Management/index.jsx
@@ -2,43 +2,41 @@ import React from 'react';
 import './style.css';
 
 export default function Management() {
-    // const renderTooltip = (type, props) => (
-    //     <Tooltip id="button-tooltip" 
-    //             {...props}>
-    //         <p className='mb-0 p-2 text-dark tooltip-text'>
-    //             {
-    //                 (
-    //                     type === 'sherman' &&
-    //                     <>
-    //                         <span className='fw-bold'>The Sherman Center</span> is a program that enables Northeastern student entrepreneurship by providing education, tools and resources. The program is designed to arm engineering students with proper entrepreneurial skills to pitch and commercialize their ideas.
-    //                     </>
-    //                 ) ||
-    //                 <>
-    //                     <span className='fw-bold'>Mosaic</span> is a group of entrepreneurially-spirited people who share a passion for building new products.They allow the community to share experiences and exchange ideas Mosaic programs distinguish Northeastern University students as leaders in entrepreneurship and innovation.
-    //                 </>
-    //             }
-    //         </p>
-    //     </Tooltip>
-    // );
+  function Color() {
+    document.getElementById('title').innerHTML= 'MANAGEMENT'
+
+  }
+  function Color2() {
+    document.getElementById('title').innerHTML = 'OPERATIONS'
+  }
+  function Color3() {
+    document.getElementById('title').innerHTML = 'SOFTWARE'
+  }
+  function Color4() {
+    document.getElementById('title').innerHTML = 'HARDWARE'
+  }
+  function Color5() {
+    document.getElementById('title').innerHTML = 'ENGAGEMENT'
+  }
+
+ 
+
+   
 
     return (
        <div>
+       <div id="skills">
+       <div class="center"></div>
+       <button id="part1" class="circle animate" onClick={Color}> </button>
+       <button id="part2" class="circle animate" onClick={Color2}> </button>
+       <button id="part3" class="circle animate" onClick={Color3}> </button>
+       <button id="part4" class="circle animate" onClick={Color4}> </button>
+       <button id="part5" class="circle animate" onClick={Color5}> </button>
+       </div>
        
-       <nav class="circle-menu">
-  <div class="center"></div>
-  <ul>
-    <li><a href="#">monitor</a></li>
-    <li><a href="#">aperture</a></li>
-    <li><a href="#">audio</a></li>
-    <li><a href="#">battery</a></li>
-    <li><a href="#">bluetooth</a></li>
-    <li><a href="#">browser</a></li>
-  </ul>
-</nav>
-
-
-
-
+       <div id="title">
+        <div>MANAGEMENT</div>
+        </div>
         </div>
     );
 }
