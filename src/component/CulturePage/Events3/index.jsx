370dc7cb04d448a4cd8e139b678b190d29fc217e
Vinay-Balaji <55262835+Vinay-Balaji@users.noreply.github.com>
2022-11-01 21:35:32 -0400

    fixed showcase

--- src/component/CulturePage/Events3/index.jsx
+++ src/component/CulturePage/Events3/index.jsx
@@ -7,25 +7,31 @@ const Events3 = () => {
       <div className="events-picture d-flex">
         <div class="row gx-5">
           <div class="col ps-5 pe-5">
-            <p className="text-uppercase all-hands-title">all-hands</p>
-            <p className="t-4 r-3 showcase-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
-              nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec
-              elit tempus risus. Tincidunt sagittis nulla orci id. Augue risus
-              adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
-              Tincidunt sagittis nulla orci id.
-            </p>
+            <div class = "all-hands">
+              <p className="text-uppercase all-hands-title">all-hands</p>
+              <p className="t-4 r-3 showcase-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
+                nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec
+                elit tempus risus. Tincidunt sagittis nulla orci id. Augue risus
+                adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
+                Tincidunt sagittis nulla orci id.
+              </p>
+            </div>
           </div>
+
           <div class="col ps-5 pe-2">
-            <p className="text-uppercase showcase-title">showcase</p>
-            <p className="t-4 r-3 showcase-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
-              nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec
-              elit tempus risus. Tincidunt sagittis nulla orci id. Augue risus
-              adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
-              Tincidunt sagittis nulla orci id.
-            </p>
+            <div class="showcase">
+              <p className="text-uppercase showcase-title">showcase</p>
+              <p className="t-4 r-3 showcase-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
+                nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec
+                elit tempus risus. Tincidunt sagittis nulla orci id. Augue risus
+                adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
+                Tincidunt sagittis nulla orci id.
+              </p>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
