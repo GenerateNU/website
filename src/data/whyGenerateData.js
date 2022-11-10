1e28f674e8a7a997761b8256f3e7f373bab06ded
Preeti Makaram <makaram.pr@northeastern.edu>
2022-11-09 20:49:41 -0500

    about page new text

--- src/data/whyGenerateData.js
+++ src/data/whyGenerateData.js
@@ -1,49 +1,47 @@
-import Course from '../assets/images/aboutpage/TheCourse.png';
-import Community from '../assets/images/aboutpage/Community.png';
-import Makerspace from '../assets/images/aboutpage/Makerspace.png';
-import Networks from '../assets/images/aboutpage/Networks.png';
-import Workshops from '../assets/images/aboutpage/Workshops.png';
-import Mission from '../assets/images/aboutpage/generate_mission.png';
-
-
+import Course from "../assets/images/aboutpage/TheCourse.png";
+import Community from "../assets/images/aboutpage/Community.png";
+import Makerspace from "../assets/images/aboutpage/Makerspace.png";
+import Networks from "../assets/images/aboutpage/Networks.png";
+import Workshops from "../assets/images/aboutpage/Workshops.png";
+import Mission from "../assets/images/aboutpage/generate_mission.png";
 
 const whyGenerateData = [
-    {
-        'section': 'Workshops',
-        'color': '#FF5751',
-        'image' : Workshops,
-        'text': 'Join frequent workshops that offer you the opportunity to learn about a variety of tools and skills from engineering to design.',
-    },
-    {
-        'section': 'Makerspace',
-        'color': '#FFBF3C',
-        'image' : Makerspace,
-        'text': 'Gain access to our Makerspace where tools like 3D printers are ready to be used for prototyping or passion projects.',
-    },
-    {
-        'section': 'Community',
-        'color': '#A559EC',
-        'image' : Community,
-        'text': 'Join a group of people who wants to learn and grow with you. Like minded individuals who share your goals and interests.',
-    },
-    {
-        'section': 'Events',
-        'color': '#56CCF2',
-        'image' : Mission,
-        'text': 'Meet other spirited individuals and enjoy bonding activities where you get to know each other beyond projects and teams.',
-    },
-    {
-        'section': 'Networks',
-        'color': '#6FCF97',
-        'image' : Networks,
-        'text': 'Access our network of great connections like Tesla and SpaceX. A unique opportunity to apply the skills you’ve learned with us.',
-    },
-    {
-        'section': 'The Course',
-        'color': '#187DFF',
-        'image' : Course,
-        'text': 'Do you want to skip a dreaded elective? Recieve credits working on projects while learning all about product development!',
-    },
+  {
+    section: "Workshops",
+    color: "#FF5751",
+    image: Workshops,
+    text: "Join frequent workshops that offer you the opportunity to learn about a variety of tools and skills from engineering to design.",
+  },
+  {
+    section: "Makerspace",
+    color: "#FFBF3C",
+    image: Makerspace,
+    text: "Gain access to our Makerspace where tools like 3D printers are ready to be used for prototyping or passion projects.",
+  },
+  {
+    section: "Community",
+    color: "#A559EC",
+    image: Community,
+    text: "Join a group of people who wants to learn and grow with you. Like minded individuals who share your goals and interests.",
+  },
+  {
+    section: "Events",
+    color: "#56CCF2",
+    image: Mission,
+    text: "Meet other excited individuals and enjoy bonding activities where you get to know each other beyond projects and teams.",
+  },
+  {
+    section: "Networks",
+    color: "#6FCF97",
+    image: Networks,
+    text: "Access our network of great connections like Tesla and SpaceX. A unique opportunity to apply the skills you’ve learned with us.",
+  },
+  {
+    section: "The Course",
+    color: "#187DFF",
+    image: Course,
+    text: "Do you want to skip a dreaded elective? Receive credit for Generate, working on projects while learning all about product development!",
+  },
 ];
 
 export default whyGenerateData;
