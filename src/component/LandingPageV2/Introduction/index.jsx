fed4b7b2eee61f185e06366f96915fc7c3c73d9d
Preeti Makaram <makaram.pr@northeastern.edu>
2022-11-15 22:14:02 -0500

    navbar and scroll image

--- src/component/LandingPageV2/Introduction/index.jsx
+++ src/component/LandingPageV2/Introduction/index.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import "./style.css";
-
+import NavBar from "../../NavBar";
+import ScrollButton from "../../../assets/images/landingpage-v2/scroll button.png";
 
 export default function Introduction() {
-    return (
-      <div className="introductionContainer">
+  return (
+    <div className="introductionContainer">
+      <div className="shermancenter">
+        <NavBar />
 
-               <div className="shermancenter">
-                <p className="wearetext">WE ARE</p>
-                <p className="generatetext">GENERATE</p>
-                 {/* <div className="whitebox">
+        <p className="wearetext">WE ARE</p>
+        <p className="generatetext">GENERATE</p>
+        {/* <div className="whitebox">
                  <div className="scrollarrow" ></div>
                  <p className="scrolltext">scroll</p>  
                  </div> */}
-</div>
+        <div className="scroll">
+          <img className="scrollimg" src={ScrollButton} alt="sroll left" />
+          <p className="scrolltext">scroll</p>
+        </div>
+        <div></div>
       </div>
-
-    );
+    </div>
+  );
 }
