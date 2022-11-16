6a57aa977aedb553cc50a8ce3b0bba4db33d6396
eraricamehra <mehra.e@northeastern.edu>
2022-11-16 18:18:45 -0500

    Added generic css for img

--- src/component/CulturePage/Collage/index.jsx
+++ src/component/CulturePage/Collage/index.jsx
@@ -6,32 +6,32 @@ export default function collage() {
     <div>
 <div class="flex-parent">
   <div class="flex-child">
-  <img class="img1" src="https://images.unsplash.com/photo-1530104091755-015d31dfa0b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1418&q=80">
+  <img class="img1 culture-general-img" src="https://images.unsplash.com/photo-1530104091755-015d31dfa0b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1418&q=80">
+</img> 
+  </div>
+  <div class="flex-child">
+  <img className = "culture-general-img" src="https://images.unsplash.com/photo-1523825939563-3d119108584d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80">
 </img>
   </div>
   <div class="flex-child">
-  <img src="https://images.unsplash.com/photo-1523825939563-3d119108584d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80">
-</img>
-  </div>
-  <div class="flex-child">
-  <img src="https://images.unsplash.com/photo-1577085894322-2c9e824c270e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80">
+  <img className = "culture-general-img"  src="https://images.unsplash.com/photo-1577085894322-2c9e824c270e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80">
 </img>
   </div>
 </div>
 <div class="flex-parent">
-  <div class="flex-child"> <img class="img2" src="https://images.unsplash.com/photo-1465488867967-ffb57e7f0a89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
+  <div class="flex-child"> <img class="img2 culture-general-img" src="https://images.unsplash.com/photo-1465488867967-ffb57e7f0a89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img></div>
-  <div class="flex-child"><img src="https://images.unsplash.com/photo-1530453029958-05ad3266af54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1710&q=80">
+  <div class="flex-child"><img className = "culture-general-img"  src="https://images.unsplash.com/photo-1530453029958-05ad3266af54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1710&q=80">
 </img></div>
-  <div class="flex-child"><img src="https://images.unsplash.com/photo-1503587209101-a62878726384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
+  <div class="flex-child"><img className = "culture-general-img"   src="https://images.unsplash.com/photo-1503587209101-a62878726384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img></div>
 </div>
 <div class="flex-parent">
-  <div class="flex-child"> <img class="img3" src="https://images.unsplash.com/photo-1503891455988-54b97c8dfc4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
+  <div class="flex-child"> <img class="img3 culture-general-img" src="https://images.unsplash.com/photo-1503891455988-54b97c8dfc4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80">
 </img></div>
-  <div class="flex-child"><img src="https://images.unsplash.com/photo-1567866885012-3c7171d2c5ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80">
+  <div class="flex-child"><img className = "culture-general-img" src="https://images.unsplash.com/photo-1567866885012-3c7171d2c5ac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80">
 </img></div>
-  <div class="flex-child"> <img src="https://images.unsplash.com/photo-1529156362931-fa144f6d01eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1622&q=80">
+  <div class="flex-child"> <img className = "culture-general-img" src="https://images.unsplash.com/photo-1529156362931-fa144f6d01eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1622&q=80">
 </img></div>
 </div>
 
