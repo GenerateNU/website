bc5bfbfbb8c1c8f3dd25fa79b7d926811e77634c
Preeti Makaram <makaram.pr@northeastern.edu>
2022-11-17 21:00:21 -0500

    footer text changes

--- src/component/VerticalFooter/index.jsx
+++ src/component/VerticalFooter/index.jsx
@@ -14,40 +14,89 @@ export default function VerticalFooter() {
   return (
     <Container fluid className="footer-container">
       <Row>
-        <div className = "rectangleLogo">
+        <div className="rectangleLogo">
           <div className="logo-background sherm">
             <img src={GenerateLogo} height="50vh" width="50vw" />
           </div>
         </div>
       </Row>
-      <Row className="footer-text"> <a href="/about">About </a></Row>
-      <Row className="footer-text"> <a href="/culture">Culture</a></Row>
-      <Row className="footer-text"> <a href="/">Teams</a></Row>
-      <Row className="footer-text"> <a href="/">People</a></Row>
-      <Row className="footer-text"> <a href="/apply">Apply</a></Row>
-      <Row className="footer-text"> <a href="/">Projects</a></Row>
+      <Row className="footer-text">
+        {" "}
+        <a href="/about">About </a>
+      </Row>
+      <Row className="footer-text">
+        {" "}
+        <a href="/culture">Culture</a>
+      </Row>
+      <Row className="footer-text-disabled">
+        {" "}
+        <a href="/">Teams</a>
+      </Row>
+      <Row className="footer-text-disabled">
+        {" "}
+        <a href="/">People</a>
+      </Row>
+      <Row className="footer-text">
+        {" "}
+        <a href="/apply">Apply</a>
+      </Row>
+      <Row className="footer-text">
+        {" "}
+        <a href="/">Projects</a>
+      </Row>
 
       <div className="social-icons">
         <Row>
-          <Col className="icon"><a href ='https://www.google.com/'> <img src={InstaIcon} height="30vh" width="30vw" /> </a></Col>
+          <Col className="icon">
+            <a href="https://www.google.com/">
+              {" "}
+              <img src={InstaIcon} height="30vh" width="30vw" />{" "}
+            </a>
+          </Col>
         </Row>
         <Row>
-        <Col className="icon"><a href ='https://www.youtube.com/channel/UC2Y_rgZiPKPH0lSU1FyQJFw/featured'> <img src={YoutubeIcon} height="30vh" width="30vw"/> </a></Col>
-        <Col className="icon"><a href ='https://www.linkedin.com/company/generate-product-development/mycompany/'> <img src={NortheasternIcon} height="30vh" width="30vw" /></a></Col>
+          <Col className="icon">
+            <a href="https://www.youtube.com/channel/UC2Y_rgZiPKPH0lSU1FyQJFw/featured">
+              {" "}
+              <img src={YoutubeIcon} height="30vh" width="30vw" />{" "}
+            </a>
+          </Col>
+          <Col className="icon">
+            <a href="https://www.linkedin.com/company/generate-product-development/mycompany/">
+              {" "}
+              <img src={NortheasternIcon} height="30vh" width="30vw" />
+            </a>
+          </Col>
         </Row>
         <Row>
-          <Col className="icon"><a href ='https://www.linkedin.com/company/generate-product-development/mycompany/'> <img src={LinkedInIcon} height="30vh" width="30vw" /></a></Col>
-          <Col className="icon"><a href ='https://www.facebook.com/GenerateNU/'> <img src={MosaicIcon} height="30vh" width="30vw" /> </a></Col>
+          <Col className="icon">
+            <a href="https://www.linkedin.com/company/generate-product-development/mycompany/">
+              {" "}
+              <img src={LinkedInIcon} height="30vh" width="30vw" />
+            </a>
+          </Col>
+          <Col className="icon">
+            <a href="https://www.facebook.com/GenerateNU/">
+              {" "}
+              <img src={MosaicIcon} height="30vh" width="30vw" />{" "}
+            </a>
+          </Col>
         </Row>
         <Row>
-          <Col className="icon"><a href ='https://www.linkedin.com/company/generate-product-development/mycompany/'> <img src={FbIcon} height="30vh" width="30vw" /></a></Col>
-          <Col className="icon"><a href ='https://www.linkedin.com/company/generate-product-development/mycompany/'> <img src={Sherm} height="30vh" width="30vw" /></a></Col>
+          <Col className="icon">
+            <a href="https://www.linkedin.com/company/generate-product-development/mycompany/">
+              {" "}
+              <img src={FbIcon} height="30vh" width="30vw" />
+            </a>
+          </Col>
+          <Col className="icon">
+            <a href="https://www.linkedin.com/company/generate-product-development/mycompany/">
+              {" "}
+              <img src={Sherm} height="30vh" width="30vw" />
+            </a>
+          </Col>
         </Row>
-        
       </div>
-
-
     </Container>
-
   );
 }
