cd39ede8ffb79e989e4a3c1281d2b59fa4d80254
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-01-31 17:28:37 -0500

    Add mobile image and CSS

--- src/component/LandingPageV2/ProductDevLifecycle/Ideation/index.jsx
+++ src/component/LandingPageV2/ProductDevLifecycle/Ideation/index.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import DevelopmentWheel from "../ProductDevelopment/DevelopmentWheel.png"
+import DevelopmentWheelMobile from "../ProductDevelopment/DevelopmentWheelMobile.png";
 import './style.css';
 
 const Ideation = () => {
     return (
-          <div className="background-black">
-            <div className = "alignItemsRight">
-            <div className = "DevelopmentWheelTwo">
-              <img style={{width: '64vh'}}src ={DevelopmentWheel} />
-            </div>
-            <div className="ideation">
-              <div className = "column">
-                <div className="IdeationText">Ideation</div>
-                <div className= "paragraphText">Ideation involves the synthesis, definition, and validation of novel 
-                concepts as they relate to a new product. We start with problem identification to define the scope of 
-                the problem and begin market research to begin thinking about a solution. The last step of ideation 
-                is to plan out ideas until a final concept is decided, expanding upon the previous research and digging 
-                deeper into its overall viability.</div>
+      <div className="background-black">
+        <div className="alignItemsRight">
+          <div className="DevelopmentWheelTwo">
+            <img style={{ width: "64vh" }} src={DevelopmentWheel} />
+          </div>
+          <div className="DevelopmentWheelTwoMobile">
+            <img style={{ width: "30vh" }} src={DevelopmentWheelMobile} />
+          </div>
+          <div className="ideation">
+            <div className="column">
+              <div className="IdeationText">Ideation</div>
+              <div className="paragraphText">
+                Ideation involves the synthesis, definition, and validation of
+                novel concepts as they relate to a new product. We start with
+                problem identification to define the scope of the problem and
+                begin market research to begin thinking about a solution. The
+                last step of ideation is to plan out ideas until a final concept
+                is decided, expanding upon the previous research and digging
+                deeper into its overall viability.
               </div>
             </div>
-           </div>
-           </div>
-            
+          </div>
+        </div>
+      </div>
     );
 };
 export default Ideation;
