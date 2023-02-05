b864bf31ac7d2a783640ddc1a4653c1a5632d54f
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-05 04:14:17 -0500

    Refactor into separate background-black wheel and text divs within an empty div; decrease text box width

--- src/component/LandingPageV2/ProductDevLifecycle/Production/index.jsx
+++ src/component/LandingPageV2/ProductDevLifecycle/Production/index.jsx
@@ -4,19 +4,22 @@ import './style.css';
 
 const Production = () => {
     return (
-          <div className="background-black">
-            <div className = "alignItemsRight">
-            <div className = "DevelopmentWheel">
-              <img  style={{width: '64vh'}} src ={DevelopmentWheel} />
-            </div>
-            <div className="ideation">
-              <div className = "column">
-                <div className="ProductionText">Delivery</div>
-                <div className= "paragraphText">During the delivery phase, the design and engineering prototypes are merged into a cohesive design. This final product is validated to meet our clients’ needs through testing, laying the foundation for a successful launch. All of our work is thoroughly documented to ensure that our clients have the necessary resources to continue their development, or move directly into production. </div>
+          <>
+            <div className="background-black-wheel">
+              <div className = "DevelopmentWheel">
+                <img  style={{width: '64vh'}} src ={DevelopmentWheel} />
               </div>
-            </div> 
+            </div>
+            <div className="background-black-text">
+              <div className = "alignItemsRight"></div>
+              <div className="ideation">
+                <div className = "column">
+                  <div className="ProductionText">Delivery</div>
+                  <div className= "paragraphText">During the delivery phase, the design and engineering prototypes are merged into a cohesive design. This final product is validated to meet our clients’ needs through testing, laying the foundation for a successful launch. All of our work is thoroughly documented to ensure that our clients have the necessary resources to continue their development, or move directly into production. </div>
+                </div>
+              </div> 
            </div>
-           </div>
+           </>
             
     );
 };
