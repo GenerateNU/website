4f2a533726c0b67b65035d3cb586efdadd646e58
BunaAndy <eduardbuna@yahoo.com>
2023-02-09 11:53:58 -0500

    Featured Project 1 maintains ratioon resize

--- src/component/LandingPageV2/FP1/index.jsx
+++ src/component/LandingPageV2/FP1/index.jsx
@@ -6,32 +6,34 @@ import TopCorner from "../../../assets/images/landingpage-v2/top_corner.png";
 
 export default function FP1() {
   return (
-    <div className="parent">
+    <>
       <div className="column-1">
-        <div>
-          <h1 style={{fontSize: "9vh"}}>Earnz</h1>
-          <p style={{ fontFamily: "Space Mono", fontSize: "3vh" }}>
-            software
-          </p>
-        </div>
-        <div className="client">
-          <p style={{fontSize: "3vh"}} className="client-text">Client</p>
-          <div className="name-order">
-            <p style={{fontSize: "3vh"}} className="name-text">Max Thalheimer</p>
-            <p style={{fontSize: "2vh"}} className="school">Northeastern Alumnus ‘20</p>
-          </div> 
-        </div>
-        <div>
-          <p style={{width: "40vh"}} className="client-blurb">
-            A unique, two-sided promotional and loyalty platform built to level
-            the playing field for independent bars and restaurants by allowing
-            them to utilize an app to acquire and retain customers as easily and
-            cost effectively as currently only chains can.
-          </p>
-          <a href="url">learn more</a>
+        <div className="column-1-text">
+          <div>
+            <h1 style={{fontSize: "9vh"}}>Earnz</h1>
+            <p style={{ fontFamily: "Space Mono", fontSize: "3vh" }}>
+              software
+            </p>
+          </div>
+          <div className="client">
+            <p style={{fontSize: "3vh"}} className="client-text">Client</p>
+            <div className="name-order">
+              <p style={{fontSize: "3vh"}} className="name-text">Max Thalheimer</p>
+              <p style={{fontSize: "2vh"}} className="school">Northeastern Alumnus ‘20</p>
+            </div> 
+          </div>
+          <div>
+            <p style={{width: "40vh"}} className="client-blurb">
+              A unique, two-sided promotional and loyalty platform built to level
+              the playing field for independent bars and restaurants by allowing
+              them to utilize an app to acquire and retain customers as easily and
+              cost effectively as currently only chains can.
+            </p>
+            <a href="url">learn more</a>
+          </div>
         </div>
         <div className="bottom-corner">
-          <img style={{height: "25vh"}}src={BottomCorner} alt="" />
+          <img className="bottom-corner-img" src={BottomCorner} alt="" />
         </div>
       </div>
       <div className="column-2">
@@ -41,19 +43,19 @@ export default function FP1() {
       </div>
       <div className="column-3">
         <div className="top-corner">
-          <img style={{height: "25vh"}} src={TopCorner} alt="" />
+          <img className="top-corner-img" src={TopCorner} alt="" />
         </div>
-        <div className="quote-container">
-          <blockquote>
+        <blockquote>
+          <div className="quote-container">
             Generate was great as a learning experience for me, as someone who
-            hadn’t built a tech company before. The team of experienced Generate
+            hadn't built a tech company before. The team of experienced Generate
             engineers knew what it takes to build a product like earnz, how to
             package it all together, and ultimately how to come together as a
             team.
-          </blockquote>
-        </div>
+          </div>
+        </blockquote>
       </div>
-    </div>
+    </ >
   );
 }
 
