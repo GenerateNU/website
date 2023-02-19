87bff0a20579904dfc49151cae47d5c8c5fc7ff5
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-19 16:17:19 -0500

    alignItemsRight element now properly encloses text elements

--- src/component/LandingPageV2/ProductDevLifecycle/Production/index.jsx
+++ src/component/LandingPageV2/ProductDevLifecycle/Production/index.jsx
@@ -11,13 +11,14 @@ const Production = () => {
               </div>
             </div>
             <div className="background-black-text">
-              <div className = "alignItemsRight"></div>
+              <div className = "alignItemsRight">
               <div className="ideation">
                 <div className = "column">
                   <div className="ProductionText">Delivery</div>
                   <div className= "paragraphText">During the delivery phase, the design and engineering prototypes are merged into a cohesive design. This final product is validated to meet our clients’ needs through testing, laying the foundation for a successful launch. All of our work is thoroughly documented to ensure that our clients have the necessary resources to continue their development, or move directly into production. </div>
                 </div>
               </div> 
+              </div>
            </div>
            </>
             
