81de28cd599c09df0107c259a0b90a53e6592c7e
garrettladley <ladley.g@northeastern.edu>
2023-02-19 18:13:07 -0500

    initial refactoring...

--- src/component/AboutPage/Stats/index.jsx
+++ src/component/AboutPage/Stats/index.jsx
@@ -1,29 +1,26 @@
 import React from "react";
+import "./style.css";
 
 export default function Stats() {
   return (
     <div className="position-relative w-75 vh-100 d-flex bg-white">
-      <div className="members">
-        <div className="today">
-          <h3>TODAY</h3>
+      <div className="todayContainer">
+        <p className="today">TODAY</p>
+      </div>
+      <div className="statsContainer">
+        <p className="statTtile">Members</p>
+        <p className="stateNumber">135</p>
+        <div className="clients">
+          <p className="statTtile">Clients</p>
+          <p className="stateNumber">47</p>
         </div>
-        <div className="membersInfo">
-          <div className="membersInfo-Members">
-            <h1>Members</h1>
-            <p>112</p>
-          </div>
-          <div className="membersInfo-Clients">
-            <h1>Clients</h1>
-            <p>47</p>
-          </div>
-          <div className="membersInfo-Teams">
-            <h1>Teams</h1>
-            <p>22</p>
-          </div>
-          <div className="membersInfo-Roles">
-            <h1>Roles</h1>
-            <p>44</p>
-          </div>
+        <div className="teams">
+          <p className="statTtile">Teams</p>
+          <p className="stateNumber">22</p>
+        </div>
+        <div className="roles">
+          <p className="statTtile">Roles</p>
+          <p className="stateNumber">44</p>
         </div>
       </div>
     </div>
