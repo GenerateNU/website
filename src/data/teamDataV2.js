30e40db510ca9343b1aa6df5c51dfdf0909ab419
BunaAndy <eduardbuna@yahoo.com>
2023-02-27 17:56:47 -0500

    Add teams data v2 and basic map for page

---
+++ src/data/teamDataV2.js
@@ -0,0 +1,45 @@
+import ManagementActiveIcon from "../assets/icons/teams/colored/Management.png";
+import OperationsActiveIcon from "../assets/icons/teams/colored/Operations.png";
+import SoftwareActiveIcon from "../assets/icons/teams/colored/Software.png";
+import HardwareActiveIcon from "../assets/icons/teams/colored/Hardware.png";
+import CommunityActiveIcon from "../assets/icons/teams/colored/Community.png";
+import ManagementInactiveIcon from "../assets/icons/teams/gray/Management.png";
+import OperationsInactiveIcon from "../assets/icons/teams/gray/Operations.png";
+import SoftwareInactiveIcon from "../assets/icons/teams/gray/Software.png";
+import HardwareInactiveIcon from "../assets/icons/teams/gray/Hardware.png";
+import CommunityInactiveIcon from "../assets/icons/teams/gray/Community.png";
+
+const teamDataV2 = [
+    {
+        team: "Management",
+        color: "#FFBF3C",
+        activeIcon: ManagementActiveIcon,
+        inactiveIcon: ManagementInactiveIcon,
+    },
+    {
+        team: "Operations",
+        color: "#A559EC",
+        activeIcon: CommunityActiveIcon,
+        inactiveIcon: CommunityInactiveIcon,
+    },
+    {
+        team: "Software",
+        color: "#187DFF",
+        activeIcon: SoftwareActiveIcon,
+        inactiveIcon: SoftwareInactiveIcon,
+    },
+    {
+        team: "Hardware",
+        color: "#FF5751",
+        activeIcon: HardwareActiveIcon,
+        inactiveIcon: HardwareInactiveIcon,
+    },
+    {
+        team: "Engagement",
+        color: "#6FCF97",
+        activeIcon: OperationsActiveIcon,
+        inactiveIcon: OperationsInactiveIcon,
+    }
+]
+
+export default teamDataV2;
