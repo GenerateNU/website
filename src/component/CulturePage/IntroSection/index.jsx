bc5b5f95a363769bbdfe219aa2eb7a1bcfd675bb
garrettladley <ladley.g@northeastern.edu>
2023-02-26 19:03:05 -0500

    more tweaks

--- src/component/CulturePage/IntroSection/index.jsx
+++ src/component/CulturePage/IntroSection/index.jsx
@@ -7,7 +7,7 @@ export default function IntroSection() {
   return (
     <div className="position-relative w-80 vh-100">
       <div className="intro-navbar">
-        <NavBar />{" "}
+        <NavBar />
       </div>
 
       <div className="w-50 d-flex">
@@ -16,10 +16,11 @@ export default function IntroSection() {
         </div>
         <div className="culture-quote-positioning p-3">
           <div className="our-culture-quote">
-            We make wicked projects and plan wicked events
+            We make wicked projects &amp; plan wicked events
           </div>
         </div>
       </div>
+
       <h1 className="our-mission-header text-no-fill text-uppercase position-absolute start-0 bottom-0 p-5 fi">
         Our Culture
       </h1>
