f7f931809ae46c16e093d7d1e8fb1983ef9e97b4
BunaAndy <eduardbuna@yahoo.com>
2023-02-27 17:35:17 -0500

    Routing for New Teams  Page

---
+++ src/component/TeamsPage/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './style.css';
+//import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Column from "react-bootstrap/esm/Col";
+
+export default class TeamsPage extends React.Component {
+  render() {
+    return (
+    <div
+    class="vh-100 horizontal-scroll bg-white container-fluid"
+    tab-index="0">
+        Teams WIP
+        <Row className="flex-nowrap">
+
+        </Row>
+      </div>
+    );
+  }
+};
