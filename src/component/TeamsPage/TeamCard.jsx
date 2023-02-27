eadf49b9712f163dcf2cc237f1fff997e05c46e5
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-27 18:58:49 -0500

    Proper mapping of data to team cards, proper inline styling for team card

--- src/component/TeamsPage/TeamCard.jsx
+++ src/component/TeamsPage/TeamCard.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const TeamCard = ({color = 'black', team='', ManagementActiveIcon = null}) => {
+const TeamCard = ({color = 'black', team='Team', ManagementActiveIcon = null}) => {
     //const [cardPicture, setCardPicture] = useState(props.defaultImage);
     return (
-        <div className='card-container-flex' style={"color: " + color}>
+        <div className='card-container-flex' style={{color: color}}>
             <img className='card-img' src={ManagementActiveIcon} />
             <div className='title-bkgnd'>
-                <div className='title-text' style={"color: " + color}>
+                <div className='title-text' style={{color: color}}>
                     {team}
                 </div>
             </div>
