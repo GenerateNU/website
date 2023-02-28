bb1fa2e3eaca566e26a1caba53c7c5e8434c080b
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-27 19:15:55 -0500

    Added NavBar, removed reverse() from mapping, changed colors to backgroundColors, changed font sizes

--- src/component/TeamsPage/TeamCard.jsx
+++ src/component/TeamsPage/TeamCard.jsx
@@ -4,7 +4,7 @@ import './style.css';
 const TeamCard = ({color = 'black', name='Team', ManagementActiveIcon = null}) => {
     //const [cardPicture, setCardPicture] = useState(props.defaultImage);
     return (
-        <div className='card-container-flex' style={{color: color}}>
+        <div className='card-container-flex' style={{backgroundColor: color}}>
             <img className='card-img' src={ManagementActiveIcon} />
             <div className='title-bkgnd'>
                 <div className='title-text' style={{color: color}}>
