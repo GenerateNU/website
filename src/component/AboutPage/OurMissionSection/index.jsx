aec3e19ebf9bd2a498d42c3542f5d5f5e5ceccca
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-28 18:07:24 -0500

    Added extra selector to the second quote

--- src/component/AboutPage/OurMissionSection/index.jsx
+++ src/component/AboutPage/OurMissionSection/index.jsx
@@ -6,7 +6,7 @@ import "./style.css";
 
 const OurMissionSection = () => {
   return (
-    <div className="vw-100 vh-100 py-4 px-5 mission-mobile">
+    <div className="mission-mobile vw-100 vh-100 py-4 px-5 ">
       <NavBar />
       <div className="quote-text w-75 pe-5 d-flex">
         <div className="our-mission-quotation-mark me-2">&#8220;</div>
@@ -15,7 +15,7 @@ const OurMissionSection = () => {
           experiences and to support Northeastern ventures of all backgrounds
           with their development needs.
         </div>
-        <div className="our-mission-quotation-mark pt-5">&#8221;</div>
+        <div className="our-mission-quotation-mark bottom-quote pt-5">&#8221;</div>
       </div>
       <h1 className="our-mission-header text-no-fill text-uppercase position-relative start-0 bottom-0 ps-5 pb-4">
         Our Mission
