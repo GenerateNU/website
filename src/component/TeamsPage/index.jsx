2d461193903f94172111419bbb5148782c2a1d9f
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-27 19:15:55 -0500

    Added NavBar, removed reverse() from mapping, changed colors to backgroundColors, changed font sizes

--- src/component/TeamsPage/index.jsx
+++ src/component/TeamsPage/index.jsx
@@ -4,7 +4,7 @@ import teamsDataV2 from '../../data/teamDataV2';
 import TeamCard from './TeamCard'
 //import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
-import Column from "react-bootstrap/esm/Col";
+import NavBar from "../NavBar"
 
 
 export default class TeamsPage extends React.Component {
@@ -13,12 +13,12 @@ export default class TeamsPage extends React.Component {
     <div
     class="vh-100 horizontal-scroll bg-white container-fluid"
     tab-index="0">
-        Teams WIP
         <Row className="flex-nowrap">
+        <NavBar/>
         {
                     teamsDataV2.map(team => {
                        return <TeamCard {...team}/> 
-                    }).reverse()
+                    })
                   }
         </Row>
       </div>
