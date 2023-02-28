16f8aee1f203ccb70f9b29c6ae16df6005bcbe7a
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-02-28 14:07:44 -0500

    Conditional mobile CSS and rendering of components not yet mobile-supported

--- src/component/AboutPage/index.jsx
+++ src/component/AboutPage/index.jsx
@@ -16,22 +16,48 @@ import ProductDevelopment from "../LandingPageV2/ProductDevLifecycle/ProductDeve
 import FooterTwo from "../VerticalFooterTeams";
 
 export default class AboutPage extends React.Component {
-  render() {
-    return (
-     <div class="vh-100 horizontal-scroll bg-black container-fluid" tab-index="0">
-          <Row className="flex-nowrap">
-          <OurMissionSection />
-          <WeAreSection />
-          <Leadership />
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
 
-          <History />
-          <Timeline />
-          <Stats/>
-          <WhyGenerateCarousel />
-          <GrowthMantra/>
-          <Footer />
-          <FooterTwo />
-          
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  
+  // make sure to remove the listener
+  // when the component is not mounted anymore
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+  
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+    this.forceUpdate();
+  };
+
+
+  render() {
+    const width = this.state.width;
+    const isMobile = width <= 650;
+    return (
+     <div class={`${isMobile ? "vw-100 vertical-scroll" : "vh-100 horizontal-scroll"} bg-black container-fluid`} tab-index="0">
+          <Row className="flex-nowrap">
+            <OurMissionSection />
+            <WeAreSection />
+            {!isMobile && <>
+              <Leadership />
+              <History />
+              <Timeline />
+              <Stats/>
+              <WhyGenerateCarousel />
+              <GrowthMantra/>
+              <Footer />
+              <FooterTwo />
+              </>
+            }
           </Row>
       </div>
 
