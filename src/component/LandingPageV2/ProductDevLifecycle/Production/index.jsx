349b9879fa30886ba4b0be02ecfa53e726453757
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-02 13:09:53 -0500

    Refactored Production with new components

--- src/component/LandingPageV2/ProductDevLifecycle/Production/index.jsx
+++ src/component/LandingPageV2/ProductDevLifecycle/Production/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import DevelopmentWheel from "../ProductDevelopment/DevelopmentWheel.png"
+import wheelImg from "../ProductDevelopment/DevelopmentWheel.png"
+import DevelopmentWheel from "../DevelopmentWheel"
+import DevelopmentText from "../DevelopmentText"
 import './style.css';
 
 const Production = () => {
-    return (
+    /*return (
           <div className="background-black">
             <div className = "alignItemsRight">
             <div className = "DevelopmentWheel">
@@ -18,6 +20,19 @@ const Production = () => {
            </div>
            </div>
             
-    );
+    ); */
+
+    const paragraphText = "During the delivery phase, the design and engineering prototypes are merged into a cohesive design. \
+                          This final product is validated to meet our clients’ needs through testing, \
+                          laying the foundation for a successful launch. All of our work is thoroughly documented \
+                          to ensure that our clients have the necessary resources to continue their development, or move directly into production." 
+    return (
+      <>
+        <DevelopmentWheel img={wheelImg} />
+        <DevelopmentText titleText={"Delivery"} paragraphText={paragraphText} titleColor={"#187DFF"} />
+      </>
+    )
 };
+
+
 export default Production;
