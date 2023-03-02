babe229252beb182b1cede49a2861dbea122a457
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-02 12:59:42 -0500

    Refactor Ideation with new components

--- src/component/LandingPageV2/ProductDevLifecycle/Ideation/index.jsx
+++ src/component/LandingPageV2/ProductDevLifecycle/Ideation/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import DevelopmentWheel from "../ProductDevelopment/DevelopmentWheel.png"
+import wheelImg from "../ProductDevelopment/DevelopmentWheel.png"
+import DevelopmentWheel from "../DevelopmentWheel"
+import DevelopmentText from "../DevelopmentText";
 import './style.css';
 
 const Ideation = () => {
-    return (
+    /*return (
       <div className="background-black">
         <div className="alignItemsRight">
           <div className="DevelopmentWheelTwo">
@@ -11,7 +13,7 @@ const Ideation = () => {
           </div>
           {/* <div className="DevelopmentWheelTwoMobile">
             <img style={{ width: "30vh" }} src={DevelopmentWheelMobile} />
-          </div> */}
+          </div> */ /*}
           <div className="ideation">
             <div className="column">
               <div className="IdeationText">Ideation</div>
@@ -28,6 +30,20 @@ const Ideation = () => {
           </div>
         </div>
       </div>
+    );*/
+    const paragraphText = "Ideation involves the synthesis, definition, and validation of \
+    novel concepts as they relate to a new product. We start with \
+    problem identification to define the scope of the problem and \
+    begin market research to begin thinking about a solution. The \
+    last step of ideation is to plan out ideas until a final concept \
+    is decided, expanding upon the previous research and digging \
+    deeper into its overall viability."
+
+    return (
+      <>
+        <DevelopmentWheel img={wheelImg} />
+        <DevelopmentText titleText={"Ideation"} paragraphText ={paragraphText} titleColor = {"#FFBF3C"} />
+      </>
     );
 };
 export default Ideation;
