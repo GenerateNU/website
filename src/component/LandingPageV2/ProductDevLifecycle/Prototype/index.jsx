41866be20dec24fc9951d85401fad47c0322f987
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-02 13:05:21 -0500

    Refactored Prototype with new components

--- src/component/LandingPageV2/ProductDevLifecycle/Prototype/index.jsx
+++ src/component/LandingPageV2/ProductDevLifecycle/Prototype/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import DevelopmentWheel from "../ProductDevelopment/DevelopmentWheel.png"
+import wheelImg from "../ProductDevelopment/DevelopmentWheel.png"
+import DevelopmentWheel from "../DevelopmentWheel"
+import DevelopmentText from "../DevelopmentText"
 import './style.css';
 
 const Prototype = () => {
-    return (
+    /*return (
           <div className="background-black">
             <div className = "alignItemsRight">
             <div className = "DevelopmentWheelThree">
@@ -18,6 +20,18 @@ const Prototype = () => {
            </div>
            </div>
             
+    );*/
+    const paragraphText = "From the background research and concept generation in the ideation phase, \
+                          the concept is ready for prototyping and initial implementation. \
+                          The Prototyping phase has two main facets: design and engineering. \
+                          The ‘design’ aspect covers the user experience and interaction with the product, \
+                          while the ‘engineering’ aspect focuses on the specific engineering implementation of the concept. \
+                          Both of these elements provide valuable insights which inform the final direction of the product."
+    return (
+      <>
+        <DevelopmentWheel img={wheelImg} />
+        <DevelopmentText titleText={"Prototype"} paragraphText = {paragraphText} titleColor = {"#FF6660"} />
+      </>
     );
 };
 export default Prototype;
