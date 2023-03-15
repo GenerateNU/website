65d4f8c9067bc08cfdf61e815ebbca159aa97166
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-03-15 12:16:13 -0400

    Add back JSX file

---
+++ src/component/NextPage/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./style.css";
+import Arrow from "../../assets/images/nextpage/arrow.svg";
+
+export default function NextPage(props) {
+  return (
+    <div className="nextPage">
+      <a href={props.url}>
+        <button className="nextPageButton">
+          <img src={Arrow} className={"nextPageArrow"}></img>
+          <span className={"nextPageName"}>{props.pageName}</span>
+          <img src={Arrow} className={"nextPageArrow"}></img>
+        </button>
+      </a>
+    </div>
+  );
+}
