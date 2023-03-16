7499d69a4194328353c4a9d8c954d6c9e7b28713
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-16 13:10:06 -0400

    Wrapped in prev-proj-wrapper

--- src/component/LandingPageV2/PreviousProjects/index.jsx
+++ src/component/LandingPageV2/PreviousProjects/index.jsx
@@ -36,10 +36,10 @@ export default function PreviousProjects() {
   ));
 
   return (
-    <>
+    <div className="prev-proj-wrapper">
       <div className="col-1">{l1}</div>
       <div className="col-2">{l2}</div>
       <div className="col-3">{l3}</div>
-    </>
+    </div>
   );
 }
