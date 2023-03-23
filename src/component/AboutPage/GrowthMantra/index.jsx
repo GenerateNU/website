b6513cace50130ae29bcb7c470cc38bd2cead6f3
rhoyler7 <roberthoyler@gmail.com>
2023-03-23 15:48:06 -0400

    Edits #2

--- src/component/AboutPage/GrowthMantra/index.jsx
+++ src/component/AboutPage/GrowthMantra/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cooltriangle from "../../AboutPage/GrowthMantra/Green.png";
 import leftcarrot from "../../AboutPage/GrowthMantra/chevron.png";
-import mountains from "../../AboutPage/GrowthMantra/Mountains.png";
+import mountains from "../../AboutPage/GrowthMantra/Mountains.svg";
 import "./style.css";
 
 
