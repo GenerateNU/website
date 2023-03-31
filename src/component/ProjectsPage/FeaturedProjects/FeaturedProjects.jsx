c853805d39e691fc59b252a53ba5ecaee5459f2d
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-03-30 20:39:15 -0400

    Add project stuff

--- src/component/ProjectsPage/FeaturedProjects/FeaturedProjects.jsx
+++ src/component/ProjectsPage/FeaturedProjects/FeaturedProjects.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../../NavBar";
 import "./style.css";
 import Arrow from "../../../assets/images/projectspage/arrowbutton.svg";
+import EarnzMockUp from "../../../assets/images/landingpage-v2/Earnz_Mock_Up.png";
 
 export default function FeaturedProjects() {
   return (
@@ -15,14 +16,36 @@ export default function FeaturedProjects() {
             Teams work side-by-side to ensure products and processes are beyond
             expectations
           </p>
+          <div className="featured-project-buttons">
+            <button
+              className="projects-button projects-up"
+              onClick={console.log("up")}
+            >
+              <img src={Arrow}></img>
+            </button>
+            <button
+              className="projects-button projects-down"
+              onClick={console.log("down")}
+            >
+              <img src={Arrow}></img>
+            </button>
+          </div>
         </div>
-        <div className="featured-project-buttons">
-          <button className="projects-button projects-up" onClick={console.log("up")}>
-            <img src={Arrow}></img>
-          </button>
-          <button className="projects-button projects-down" onClick={console.log("down")}>
-            <img src={Arrow}></img>
-          </button>
+        <div className="projects">
+          <div className="project">
+            <img src={EarnzMockUp}></img>
+            <div className="project-text">
+              <h1 className="earnz-header">Earnz</h1>
+              <h2 className="project-team">software</h2>
+              <p>
+                A unique, two-sided promotional and loyalty platform built to
+                level the playing field for independent bars and restaurants by
+                allowing them to utilize an app to acquire and retain customers
+                as easily and cost effectively as currently only chains can.
+              </p>
+              <button className="view-project-button">View</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
