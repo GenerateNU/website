4d5a763f671420afb96066bfb02088f623da9a70
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-04-06 08:27:01 -0400

    Initial events images page resizing

--- src/component/CulturePage/Events/index.jsx
+++ src/component/CulturePage/Events/index.jsx
@@ -6,39 +6,39 @@ import RectImg3 from "./website-7.jpg";
 import { DialogContent } from "@material-ui/core";
 
 const Events = () => {
+    const imgWidth = 25; //the value by which to scale the viewport height to get the image width
+    const imgHeight = 1.4*imgWidth;
   return (
-    <div className="position-relative vw-100 vh-100 py-4 px-5">
-      <div className="w-75 pe-5 d-flex">
+    <>
+    <div className="events-container position-relative vh-100 py-4 px-5">
+      <div className="w-100 pe-5 d-flex">
         <div className="events-top">celebrate &amp; represent</div>
       </div>
+      <div className="events-header text-no-fill text-uppercase start-0 pb-4">events</div>
 
-      <div className="events-header text-no-fill text-uppercase position-absolute start-0 bottom-1 ps-4 pb-4">
-      {/* <div className="eventsTitle"> */}
-        events
-      </div>
-
-      <div className="events-picture-container d-flex flex-wrap justify-content-between">
+      <div className="events-picture-container row gx-5 d-flex lex-wrap justify-content-between py-5">
           {/* <div className="for-padding"> */}
-            <div class="row gx-5">
-              <div class="col">
-                <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg1} alt="" />
+              <div class="col-4">
+                <img className="img-formatting" style={{ width: `${imgWidth}vmin`, height: `${imgHeight}vmin`}} src={RectImg1} alt="" />
               </div>
         {/* </div> */}
-          <div class="col">
-            <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg2} alt="" />
+          <div class="col-4">
+            <img className="img-formatting" style={{ width: `${imgWidth}vmin`, height: `${imgHeight}vmin`}} src={RectImg2} alt="" />
           </div>
-          <div class="col">
-            <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg3} alt="" />
+          <div class="col-4">
+            <img className="img-formatting" style={{ width: `${imgWidth}vmin`, height: `${imgHeight}vmin`}} src={RectImg3} alt="" />
           </div>
-          <div class="col">
+                  </div>
+      </div>
+      
+    <div class="h-75 w-25 pt-5">
             <p className="text-uppercase retreat-title">retreat</p>
             <p className="retreat-text">
               Retreat is the ultimate team-bonding experience, and is everyones favorite event of the year. We spend the night in a house up north, and get to know each other! You can expect walks in the woods, playing games, home-cooked meals, and stories to laugh about for years to come.
             </p>
           </div>
-        </div>
-      </div>
-    </div>
+
+    </>
   );
 };
 
