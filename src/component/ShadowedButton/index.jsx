87a6fb8a028c6662c47e48b01b7f01c5a3210d8b
garrettladley <ladley.g@northeastern.edu>
2023-04-11 19:33:50 -0400

    formatted w/ prettier

--- src/component/ShadowedButton/index.jsx
+++ src/component/ShadowedButton/index.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 import { constants } from "../../assets/constants.js";
 
 export default function ShadowedButton(props) {
+  const ButtonStyle = {
+    backgroundColor: props.fillColor,
+    padding: `${props.yPad || "1rem"} ${props.xPad}`,
+    fontSize: `${props.fontSize || "1.5vw"}`,
+  };
 
-    const ButtonStyle = {
-        backgroundColor: props.fillColor,
-        padding: `${props.yPad || '1rem'} ${props.xPad}`,
-        fontSize: `${props.fontSize || '1.5vw'}`
-    };
+  const handleOnClick = () => {
+    const name = props.lnk;
+    const key = name.replace(" ", "_");
+    const POSITIONS_URL = "Position_" + key;
+    window.location.assign(constants[POSITIONS_URL]);
+  };
 
-    const handleOnClick=() => {
-       const name = props.lnk;
-       const key = name.replace(" ", "_");
-       const POSITIONS_URL = 'Position_' + key;
-       window.location.assign(constants[POSITIONS_URL]);
-    }
-
-    return (
-        <button className={`button-style fit-content me-5 text-nowrap ${props.className}`}
-                style={ButtonStyle}
-                onClick={props.onClick ? props.onClick : handleOnClick}>
-            {props.text}
-        </button>
-    );
+  return (
+    <button
+      className={`button-style fit-content me-5 text-nowrap ${props.className}`}
+      style={ButtonStyle}
+      onClick={props.onClick ? props.onClick : handleOnClick}
+    >
+      {props.text}
+    </button>
+  );
 }
