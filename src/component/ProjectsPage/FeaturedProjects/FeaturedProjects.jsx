10dfcac8707263b2d6dc6c922d76ddc019babf5b
garrettladley <ladley.g@northeastern.edu>
2023-04-12 17:28:26 -0400

    max image size logic

--- src/component/ProjectsPage/FeaturedProjects/FeaturedProjects.jsx
+++ src/component/ProjectsPage/FeaturedProjects/FeaturedProjects.jsx
@@ -11,6 +11,15 @@ export default function FeaturedProjects() {
     const currentIndex = Projects.indexOf(currentProject);
     setCurrentProject(Projects[(currentIndex + dir + Projects.length) % Projects.length]) 
   }
+  const maxImage = Projects.map(project => project.image).reduce((max, image) => {
+    const imgObj = new Image();
+    imgObj.src = image;
+    if (imgObj.naturalWidth > max.width && imgObj.naturalHeight > max.height) {
+      return { width: imgObj.naturalWidth, height: imgObj.naturalHeight };
+    }
+    return max;
+  }, { width: 0, height: 0 });
+
 
   return (
     <div className="projects-page-container">
@@ -27,7 +36,7 @@ export default function FeaturedProjects() {
           <div className="featured-project-buttons">
             <div className="projects-mobile">
               <h1>{currentProject.name}</h1>
-              <h2>{currentProject.type}</h2>
+              <h2 className="project-team">{currentProject.type}</h2>
             </div>
             <div className="projects-buttons">
               <button
@@ -47,7 +56,9 @@ export default function FeaturedProjects() {
         </div>
         <div className="projects">
           <div className="project">
-            <img src={currentProject.image}></img>
+            <div className="image-container" style={{width:maxImage.width, height:maxImage.height}}>
+              <img src={currentProject.image}></img>
+            </div>
             <div className="project-text">
               <h1 className="projects-desktop">{currentProject.name}</h1>
               <h2 className="project-team projects-desktop">
