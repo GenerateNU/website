6c2ac4797272bdbb4698af0902f933c6b9ebe03d
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-04-12 20:39:00 -0400

    Brought value cards out of the div

--- src/component/AboutPage/WeAreSection/index.jsx
+++ src/component/AboutPage/WeAreSection/index.jsx
@@ -6,25 +6,18 @@ import './style.css';
 const WeAreSection = () => {
     return (
         <> 
-             <div >
              <div className='mission-header-space'></div>
-                <div className="weare-header fit-content">
+                <div className="weare-header">
                     <h5 className="mb-0 our-mission-our-values we-are-values w-auto h-auto text-end p-4">
                         our values
                     </h5>
-                    <h1 className="our-mission-we-are text-no-fill text-uppercase fit-content">
+                    <h1 className="our-mission-we-are text-no-fill text-uppercase text-nowrap fit-content">
                         We are
                     </h1>
                 </div>
-                <div className="we-are-header-space"></div>
-                <div className="card-container d-flex align-items-center">
-                    {
-                        valueData.map(value => <ValueCard value={value} />)
-                    }
-                </div>
-            </div>
-            <div className="cards-space"></div>
-
+            {
+                valueData.map(value => <div className="desktop-card-container"><ValueCard value={value}/></div>)
+            }
          
            
         </>
