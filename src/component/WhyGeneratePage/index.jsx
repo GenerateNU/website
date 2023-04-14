fb3e822bc99872f9aee4c125411a62fb3285030c
Quang Ngo <giaquang.education@gmail.com>
2023-04-14 16:06:01 -0400

    Starting to make mobile page for about why generate

---
+++ src/component/WhyGeneratePage/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+import React from "react";
+
+export default function WhyGeneratePage() {
+    return (
+        <div className='mobile'>
+            <p className='moregoodiestext'>more goodies</p>
+            <div>
+                <p>WHY</p>
+                <p>GENERATE ?</p>
+            </div>
+            <img src='#'/>
+            
+        </div>
+    );
+}
