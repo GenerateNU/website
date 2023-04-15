c3ddeb9ed279f8da96c6d628f5a0db5492ad28fe
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-04-15 12:29:41 -0400

    Disable links for now

--- src/component/ProjectsPage/AllProjects/Project/Project.jsx
+++ src/component/ProjectsPage/AllProjects/Project/Project.jsx
@@ -5,7 +5,8 @@ export default function Project({name, image, teams, ...props}) {
       <div className="project-card">
         <div className="project-info">
           <h2 className="project-name">{name}</h2>
-          <a className="project-link" href="/projects">
+          {/* disabled links until page for individual projects is built*/}
+          <a style={{pointerEvents: 'none'}} className="project-link" href="/projects">
             {/* placeholder */}
             <svg width="100%" height="100%">
               <rect width="100%" height="100%" fill="red"></rect>
