579a936b636d96d113ac762d6df0202142972b15
Quang Ngo <giaquang.education@gmail.com>
2023-04-15 14:56:21 -0400

    Completed the CSS for mobile page

--- src/component/AboutPage/WhyGenerateCarousel/index.jsx
+++ src/component/AboutPage/WhyGenerateCarousel/index.jsx
@@ -62,31 +62,82 @@ export default function WhyGenerateCarousel() {
                         </h1>
                     </div>
                     <div className="h-100">
-                        <div className='pe-3 h-100'>
+                        <div>
                             <Image src={whyGenerateData[currentSection].image} className='why-gen-fit-image'/>
                             <p className="why-gen-text-mobile">
                                 {whyGenerateData[currentSection].text}
                             </p>
                         </div>
-                        <div className='ps-5'>
-                            {
-                                whyGenerateData.map((d, index) =>
-                                    <div className='mb-5'
-                                         onMouseOver={() => setCurrentSection(index)}>
-                                        <h3 className="m-0 p-0 fit-content why-gen-section"
-                                            style={{color: currentSection === index ? d.color : 'white'}}>
-                                                        <span
-                                                            className={currentSection !== index ? 'd-none' : 'd-inline me-4'}>
-                                                            &gt;
+                        <div>
+                            <div className='row mb-5'>
+                                <div className='col'
+                                     onMouseOver={() => setCurrentSection(0)}>
+                                    <h3 className="m-0 p-0 fit-content why-gen-section"
+                                        style={{color: currentSection === 0 ? whyGenerateData[0].color : 'white',
+                                        textDecoration: currentSection === 0 ? 'underline' : 'none'}}>
+                                        <span
+                                            className={`text-nowrap`}>
+                                                            {whyGenerateData[0].section}
                                                         </span>
-                                            <span
-                                                className={`text-nowrap ${currentSection !== index ? 'ms-5 ps-2' : ''}`}>
-                                                            {d.section}
+                                    </h3>
+                                </div>
+                                <div className='col'
+                                     onMouseOver={() => setCurrentSection(3)}>
+                                    <h3 className="m-0 p-0 fit-content why-gen-section"
+                                        style={{color: currentSection === 3 ? whyGenerateData[3].color : 'white',
+                                            textDecoration: currentSection === 3 ? 'underline' : 'none'}}>
+                                        <span
+                                            className={`text-nowrap`}>
+                                                            {whyGenerateData[3].section}
                                                         </span>
-                                        </h3>
-                                    </div>
-                                )
-                            }
+                                    </h3>
+                                </div>
+                            </div>
+                            <div className='row mb-5'>
+                                <div className='col'
+                                     onMouseOver={() => setCurrentSection(1)}>
+                                    <h3 className="m-0 p-0 fit-content why-gen-section"
+                                        style={{color: currentSection === 1 ? whyGenerateData[1].color : 'white',
+                                            textDecoration: currentSection === 1 ? 'underline' : 'none'}}>
+                                        <span
+                                            className={`text-nowrap`}>
+                                                            {whyGenerateData[1].section}
+                                                        </span>
+                                    </h3>
+                                </div>
+                                <div className='col'
+                                     onMouseOver={() => setCurrentSection(4)}>
+                                    <h3 className="m-0 p-0 fit-content why-gen-section"
+                                        style={{color: currentSection === 4 ? whyGenerateData[4].color : 'white',
+                                            textDecoration: currentSection === 4 ? 'underline' : 'none'}}>
+                                        <span
+                                            className={`text-nowrap`}>
+                                                            {whyGenerateData[4].section}
+                                                        </span>
+                                    </h3>
+                                </div>
+                            </div>
+                            <div className='row mb-5'>
+                                <div className='col'
+                                     onMouseOver={() => setCurrentSection(2)}>
+                                    <h3 className="m-0 p-0 fit-content why-gen-section"
+                                        style={{color: currentSection === 2 ? whyGenerateData[2].color : 'white',
+                                            textDecoration: currentSection === 2 ? 'underline' : 'none'}}>
+                                        <span
+                                            className={`text-nowrap`}>
+                                                            {whyGenerateData[2].section}
+                                                        </span>
+                                    </h3>
+                                </div>
+                                <div className='col'
+                                     onMouseOver={() => setCurrentSection(5)}>
+                                    <h3 className="m-0 p-0 fit-content why-gen-section"
+                                        style={{color: currentSection === 5 ? whyGenerateData[5].color : 'white',
+                                            textDecoration: currentSection === 5 ? 'underline' : 'none'}}>
+                                        <span className={`text-nowrap`}>{whyGenerateData[5].section}</span>
+                                    </h3>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
