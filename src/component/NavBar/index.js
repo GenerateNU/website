9a2836b6f582892e3368a1b2c4d362514462ea09
garrettladley <ladley.g@northeastern.edu>
2023-04-16 01:29:26 -0400

    implemented desktop nav menu | TODO: fix apply page mobile | TODO: newsletter redirect | TODO: code cleanup

--- src/component/NavBar/index.js
+++ src/component/NavBar/index.js
@@ -4,9 +4,8 @@ import GenerateLogo from "../../assets/images/landingpage/affiliateorgintros/Gen
 import MenuIcon from "@material-ui/icons/Menu";
 import "./style.css";
 import Modal from "react-bootstrap/Modal";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { useNavigate } from "react-router-dom";
+import Arrow from "../../assets/images/projectspage/arrowbutton.svg";
 
 const NavBar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -21,11 +20,11 @@ const NavBar = () => {
 
   const nav = isBigScreen ? (
     <>
-      {/* Todo : change the logo */}
       <img
         style={{ width: "min(3.5vmin, 40px)", height: "auto" }}
         src={GenerateLogo}
         onClick={navigateHome}
+        alt="Generate Logo"
       />
       <span
         className="font-size bg-transparent shadow-none text-black border-0 text-decoration-none text-uppercase cursor-pointer menu-hover align-item-center"
@@ -54,47 +53,6 @@ const NavBar = () => {
     </>
   );
 
-  const MenuItem = ({ href, text }) => {
-    return (
-      <Row className="m-0 p-0">
-        <a className="header-font-style" href={href}>
-          {text}
-        </a>
-      </Row>
-    );
-  };
-
-  const MenuColumn = ({ items }) => {
-    return (
-      <Col xs={5} className="m-0 p-0">
-        {items.map((item) => (
-          <MenuItem key={item.href} href={item.href} text={item.text} />
-        ))}
-      </Col>
-    );
-  };
-
-  const FlexItem = () => {
-    return <div className="item"></div>;
-  };
-
-  const FlexBox = () => {
-    return (
-      <div className="flexbox">
-        <FlexItem />
-        <FlexItem />
-        <FlexItem />
-
-        <div className="flexbox2">
-          <FlexItem />
-          <FlexItem />
-          <FlexItem />
-          <FlexItem />
-        </div>
-      </div>
-    );
-  };
-
   const menuItemsLeft = [
     { href: "/", text: "Generate" },
     { href: "/about", text: "About" },
@@ -109,53 +67,195 @@ const NavBar = () => {
 
   const LEFT_V_LINE = 5;
   const RIGHT_V_LINE = 95;
+  const DESKTOP_RIGHT_V_LINE = 90;
   const TOP_H_LINE = 5;
   const BOTTOM_H_LINE = 90;
   const MOBILE_MENU_TEXT_LEFT = 12.5;
   const MOBILE_MENU_TEXT_TOP = 6;
+  const MOBILE_N_DOTS = 7;
+  const DESKTOP_N_DOTS = 4;
   const YELLOW = "#ffbf3c";
 
   const modalContents = isBigScreen ? (
     <>
-      <Modal.Header closeButton className="m-0" />
-      <Modal.Body className="m-0 px-5 overflow-hidden text-black">
-        <Row className="m-0 p-0 menu-font-style ps-5 vh-45 text-uppercase">
-          Menu
-        </Row>
-        <Row className="m-0 p-0 vh-40 ps-5">
-          <MenuColumn items={menuItemsLeft} />
-          <MenuColumn items={menuItemsRight} />
-        </Row>
-        <Col>
-          <div className="v1" />
-        </Col>
-        <hr
-          style={{
-            background: "black",
-            color: "black",
-            borderColor: "black",
-            height: "2px",
-            marginRight: "15px",
-            width: "px",
-          }}
-        />
-        <FlexBox />
-        <div className="rectangleLogo" />
-      </Modal.Body>
-    </>
-  ) : (
-    <>
-      <div className="h-line" style={{ top: `${TOP_H_LINE}%` }} />
-      <div className="v-line" style={{ left: `${LEFT_V_LINE}%` }} />
-      <div className="v-line" style={{ left: `${RIGHT_V_LINE}%` }} />
-      <div className="h-line" style={{ top: `${BOTTOM_H_LINE}%` }} />
+      <div
+        className="v-line"
+        style={{ left: `${LEFT_V_LINE}%`, height: "90%" }}
+      />
+      <div
+        className="v-line"
+        style={{
+          top: `${BOTTOM_H_LINE}%`,
+          left: `${LEFT_V_LINE}%`,
+          height: "90%",
+        }}
+      />
+      <div
+        className="v-line"
+        style={{ left: `${DESKTOP_RIGHT_V_LINE}%`, height: "90%" }}
+      />
+      <div
+        className="v-line"
+        style={{
+          top: `${BOTTOM_H_LINE}%`,
+          left: `${DESKTOP_RIGHT_V_LINE}%`,
+          height: "90%",
+        }}
+      />
+      <div
+        className="h-line"
+        style={{ top: `${TOP_H_LINE}%`, width: "100%" }}
+      />
+      <div
+        className="h-line"
+        style={{ top: `${BOTTOM_H_LINE}%`, width: `${DESKTOP_RIGHT_V_LINE}%` }}
+      />
+
       <Modal.Header closeButton className="close-button" />
       <Modal.Body className="overflow-hidden text-black">
         <div
           className="menu-font-style"
           style={{
             position: "absolute",
-
+            top: `${MOBILE_MENU_TEXT_TOP}%`,
+            left: `${MOBILE_MENU_TEXT_LEFT}%`,
+          }}
+        >
+          Menu
+        </div>
+        {menuItemsLeft.map((item, index) => (
+          <a
+            href={item.href}
+            className="header-font-style"
+            style={{
+              position: "absolute",
+              top: `${MOBILE_MENU_TEXT_TOP + 45 + 8 * (1 + index)}%`,
+              left: `${MOBILE_MENU_TEXT_LEFT}%`,
+            }}
+          >
+            {item.text}
+          </a>
+        ))}
+        {menuItemsRight.map((item, index) => (
+          <a
+            href={item.href}
+            className="header-font-style"
+            style={{
+              position: "absolute",
+              top: `${MOBILE_MENU_TEXT_TOP + 45 + 8 * (1 + index)}%`,
+              left: `${MOBILE_MENU_TEXT_LEFT + 35}%`,
+            }}
+          >
+            {item.text}
+          </a>
+        ))}
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          {Array.from({ length: DESKTOP_N_DOTS }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  top: `${BOTTOM_H_LINE + 2.5}%`,
+                  left: `${
+                    ((40 - MOBILE_MENU_TEXT_LEFT - 5) / (DESKTOP_N_DOTS - 1)) *
+                      index +
+                    MOBILE_MENU_TEXT_LEFT -
+                    5
+                  }%`,
+                  position: "absolute",
+                  width: "5vh",
+                  height: "5vh",
+                  backgroundColor: YELLOW,
+                  borderRadius: "50%",
+                }}
+              />
+            );
+          })}
+          {Array.from({ length: DESKTOP_N_DOTS }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  top: `${BOTTOM_H_LINE + 2.5}%`,
+                  left: `${
+                    ((40 - MOBILE_MENU_TEXT_LEFT - 5) / (DESKTOP_N_DOTS - 1)) *
+                      index +
+                    MOBILE_MENU_TEXT_LEFT +
+                    47.5
+                  }%`,
+                  position: "absolute",
+                  width: "5vh",
+                  height: "5vh",
+                  backgroundColor: YELLOW,
+                  borderRadius: "50%",
+                }}
+              />
+            );
+          })}
+        </div>
+        {/* TODO: Implement newsletter redirect */}
+        <div
+          style={{
+            position: "absolute",
+            top: `${TOP_H_LINE}%`,
+            left: `${DESKTOP_RIGHT_V_LINE}%`,
+            width: `${100 - DESKTOP_RIGHT_V_LINE}vw`,
+            height: `${BOTTOM_H_LINE - TOP_H_LINE + 0.1}vh`,
+            backgroundColor: YELLOW,
+            border: "1px solid black",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          <div className="newsletter-font">Newsletter</div>
+        </div>
+        {/* TODO: Implement newsletter redirect */}
+        <div style={{
+            position: "absolute",
+            top: `${BOTTOM_H_LINE}%`,
+            left: `${DESKTOP_RIGHT_V_LINE}%`,
+            width: `${100 - DESKTOP_RIGHT_V_LINE}vw`,
+            height: `${100 - BOTTOM_H_LINE}vh`,
+            backgroundColor: YELLOW,
+            border: "1px solid black",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          <img src={Arrow} style={{transform:"rotate(90deg)"}}alt="Arrow"></img>
+        </div>
+      </Modal.Body>
+    </>
+  ) : (
+    <>
+      <div
+        className="h-line"
+        style={{ top: `${TOP_H_LINE}%`, width: "100%" }}
+      />
+      <div
+        className="v-line"
+        style={{ left: `${LEFT_V_LINE}%`, height: "90%" }}
+      />
+      <div
+        className="v-line"
+        style={{ left: `${RIGHT_V_LINE}%`, height: "90%" }}
+      />
+      <div
+        className="h-line"
+        style={{ top: `${BOTTOM_H_LINE}%`, width: "100%" }}
+      />
+      <Modal.Header closeButton className="close-button" />
+      <Modal.Body className="overflow-hidden text-black">
+        <div
+          className="menu-font-style"
+          style={{
+            position: "absolute",
             top: `${MOBILE_MENU_TEXT_TOP}%`,
             left: `${MOBILE_MENU_TEXT_LEFT}%`,
           }}
@@ -171,25 +271,25 @@ const NavBar = () => {
               top: `${MOBILE_MENU_TEXT_TOP + 5 + 5 * (1 + index)}%`,
               left: `${MOBILE_MENU_TEXT_LEFT}%`,
             }}
-            key={index}
           >
             {item.text}
           </a>
         ))}
         <div style={{ display: "flex" }}>
-          {Array.from({ length: 7 }).map((_, index) => {
+          {Array.from({ length: MOBILE_N_DOTS }).map((_, index) => {
             return (
               <div
                 key={index}
                 style={{
                   top: `${BOTTOM_H_LINE - 5}%`,
                   left: `${
-                    ((82.5 - MOBILE_MENU_TEXT_LEFT) / 6) * index +
+                    ((82.5 - MOBILE_MENU_TEXT_LEFT) / (MOBILE_N_DOTS - 1)) *
+                      index +
                     MOBILE_MENU_TEXT_LEFT
                   }%`,
                   position: "absolute",
-                  width: "5vw",
-                  height: "5vw",
+                  width: "4vh",
+                  height: "4vh",
                   backgroundColor: YELLOW,
                   borderRadius: "50%",
                 }}
@@ -210,20 +310,21 @@ const NavBar = () => {
         />
         {/* TODO: Implement newsletter redirect */}
         <div
-          className="newsletter-font"
           style={{
             position: "absolute",
             top: `${BOTTOM_H_LINE + 0.1}%`,
-            left: "5%",
-            width: "90vw",
-            height: "10vh",
-            paddingTop: "5%",
+            left: `${LEFT_V_LINE}%`,
+            width: `${RIGHT_V_LINE - LEFT_V_LINE}vw`,
+            height: `${100 - BOTTOM_H_LINE}vh`,
             backgroundColor: YELLOW,
             border: "1px solid black",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
             textAlign: "center",
           }}
         >
-          Newsletter
+          <div className="newsletter-font">Newsletter</div>
         </div>
         <div
           style={{
