268d4f1037630e67b15ab057f1ea7959f2ecc494
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-04-17 04:19:53 -0400

    Removed unecessary selectors and added correct ones

--- src/component/CulturePage/Events3/index.jsx
+++ src/component/CulturePage/Events3/index.jsx
@@ -5,7 +5,7 @@ const Events3 = () => {
   return (
     <div className="all-hands-div">
       <p className="text-uppercase all-hands-title">all-hands</p>
-      <p className="t-4 r-3 showcase-text">
+      <p className="all-hands-text">
         Halfway through the semester, hardware and software project teams set up demonstrations in the Sherman Center to unveil the work they’ve accomplished so far. We explore in-progress apps, gather user-testing data, play with hardware projects, and hang out with people from across the org in a fun, science-fair-esque format.
       </p>
     </div>
