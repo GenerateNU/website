78af96bcfd2fb728c033b3dacc045d7b362c20b2
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-04-17 04:01:40 -0400

    Removed bootstrap flex row, replaced with three cols in a JSX fragment

--- src/component/CulturePage/Events2/index.jsx
+++ src/component/CulturePage/Events2/index.jsx
@@ -4,27 +4,20 @@ import CelebrateRepresent from "../Events2/CelebrateRepresent.png";
 
 const Events2 = () => {
   return (
-    <div className="position-relative vw-100 vh-100 py-4 px-5">
-      <div className="events-picture-container d-flex flex-wrap justify-content-between">
-        <div class="row gx-5">
+  <>
           <div class="col ps-5 pe-5">
             <div class ="shiftLeft">
-              <div className = "MoraleBudgetTitle">
               <p className="text-uppercase moral-budget-title">
                 morale&nbsp;budget
               </p>
-              </div>
-              <div className = "MoraleBudget">
               <p className="t-4 r-3 l-5 moral-budget-text">
                 Our engagement team empowers members every semester by giving them a designated budget to spend together as teammates. What you choose to do with that budget is up to you, but you can bet itll be a good time! Teams go ax throwing, bowling, escape-room-ing, or anything else they can think of! 
               </p>
-              </div>
             </div>
           </div>
 
           <div class="col ps-5 pe-5 inclined-img-container">
             <div className="celebrate-rep-container">
-              {" "}
               <img className="inclined-image" src={CelebrateRepresent} alt="" />
             </div>
           </div>
@@ -37,30 +30,7 @@ const Events2 = () => {
             </p>
             </div>
           </div>
-
-          {/* <div class="col ps-5 pe-5">
-            <p className="text-uppercase all-hands-title">all-hands</p>
-            <p className="t-4 r-3 all-hands-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
-              nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec
-              elit tempus risus. Tincidunt sagittis nulla orci id. Augue risus
-              adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
-              Tincidunt sagittis nulla orci id.
-            </p>
-          </div> */}
-          {/* <div class="col ps-5 pe-5">
-            <p className="text-uppercase showcase-title">showcase</p>
-            <p className="t-4 r-3 showcase-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est sed
-              nunc porttitor arcu etiam. Eget duis sapien, faucibus nunc nec
-              elit tempus risus. Tincidunt sagittis nulla orci id. Augue risus
-              adipiscing mi eleifend proin nisl ac gravida. Viverra cursus.
-              Tincidunt sagittis nulla orci id.
-            </p>
-          </div> */}
-        </div>
-      </div>
-    </div>
+        </>
   );
 };
 
