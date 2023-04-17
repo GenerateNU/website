0a04ac9aabbf8ece56d4a5c9503077b02138634b
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-04-17 01:39:26 -0400

    Pulled the retreat col out of the containing div and wrapped everything in a JSX fragment; added class name to Events and image container and removed extra div around subtitle

--- src/component/CulturePage/Events/index.jsx
+++ src/component/CulturePage/Events/index.jsx
@@ -7,38 +7,39 @@ import { DialogContent } from "@material-ui/core";
 
 const Events = () => {
   return (
-    <div className="position-relative vw-100 vh-100 py-4 px-5">
-      <div className="w-75 pe-5 d-flex">
-        <div className="events-top">celebrate &amp; represent</div>
-      </div>
+    <>
+      <div className="events-container position-relative py-4 px-5">
+          <div className="events-top">celebrate &amp; represent</div>
 
-      <div className="events-header text-no-fill text-uppercase position-absolute start-0 bottom-1 ps-4 pb-4">
-      {/* <div className="eventsTitle"> */}
-        events
-      </div>
+        <div className="events-header text-no-fill text-uppercase position-absolute start-0 bottom-1 ps-4 pb-4">
+        {/* <div className="eventsTitle"> */}
+          events
+        </div>
 
-      <div className="events-picture-container d-flex flex-wrap justify-content-between">
-          {/* <div className="for-padding"> */}
-            <div class="row gx-5">
-              <div class="col">
-                <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg1} alt="" />
-              </div>
-        {/* </div> */}
-          <div class="col">
-            <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg2} alt="" />
-          </div>
-          <div class="col">
-            <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg3} alt="" />
-          </div>
-          <div class="col">
-            <p className="text-uppercase retreat-title">retreat</p>
-            <p className="retreat-text">
-              Retreat is the ultimate team-bonding experience, and is everyones favorite event of the year. We spend the night in a house up north, and get to know each other! You can expect walks in the woods, playing games, home-cooked meals, and stories to laugh about for years to come.
-            </p>
+        <div className="events-picture-container d-flex flex-wrap justify-content-between">
+            {/* <div className="for-padding"> */}
+              <div class="row gx-5">
+                <div class="col">
+                  <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg1} alt="" />
+                </div>
+          {/* </div> */}
+            <div class="col">
+              <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg2} alt="" />
+            </div>
+            <div class="col">
+              <img className="img-formatting" style={{ width: 250, height: 350 }} src={RectImg3} alt="" />
+            </div>
+            
           </div>
         </div>
       </div>
-    </div>
+      <div class="col">
+        <p className="text-uppercase retreat-title">retreat</p>
+        <p className="retreat-text">
+          Retreat is the ultimate team-bonding experience, and is everyone's favorite event of the year. We spend the night in a house up north, and get to know each other! You can expect walks in the woods, playing games, home-cooked meals, and stories to laugh about for years to come.
+        </p>
+      </div>
+    </>
   );
 };
 
