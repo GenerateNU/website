5ac9c036311342c04bc000a79330e74fb7bb57a0
Alix Heudebourg <alix.heudebourg@gmail.com>
2023-05-31 23:00:02 -0400

    hardware PL application link test

--- src/services/constants.js
+++ src/services/constants.js
@@ -32,30 +32,7 @@ export const constants = {
 
    // HW
    "Position_Hardware_Client": "https://docs.google.com/forms/d/e/1FAIpQLSc7TVI0FmZG6DujNi2GLEUeyGBQYlhAGtFUyf5AXzLIB0mF9Q/viewform?usp=sf_link",
-   "Position_Hardware_Project_Lead": "https://forms.gle/4EsrD7PHB9BqQXU26",
-   "Position_Hardware_Chiefs_Team_Lead_[NEW!]": "https://forms.gle/5n5dHh2ezb8TAcRL9",
-   "Position_Chief_Computer_Engineer": "https://forms.gle/UnqdjXzp2aYELTZb7",
-   "Position_Chief_Electrical_Engineer": "https://forms.gle/UnqdjXzp2aYELTZb7",
-   "Position_Chief_Industrial_Designer": "https://forms.gle/UnqdjXzp2aYELTZb7",
-   "Position_Chief_Mechanical_Engineer": "https://forms.gle/UnqdjXzp2aYELTZb7",
-
-   //"Position_Hardware_Designer": "",
-   //"Position_Hardware_Engineer": "",
-
-   
-   // SW
-   "Position_Software_Client": "https://docs.google.com/forms/d/e/1FAIpQLSfWrQlG1ewhGAtKYrVyLVmTQ4KPdm99j9lwm049UywL-yhL9w/viewform?usp=sf_link",
-   "Position_Software_Project_Lead": "https://forms.gle/LTE8KeCij7WLr9LdA",
-   "Position_Chief_Brand_Designer_[NEW!]": "https://forms.gle/yrr1w9u4ubnkPMpk8",
-   "Position_Chief_Frontend_Developer_[NEW!]": "https://forms.gle/yrr1w9u4ubnkPMpk8",
-   "Position_Chief_Software_Architect": "https://forms.gle/yrr1w9u4ubnkPMpk8",
-   "Position_Chief_UI/UX_Designer": "https://forms.gle/yrr1w9u4ubnkPMpk8",
-
-   // "Position_": "",
-   // "Position_": "",
-
-   // "Position_Software_Designer": "",
-   // "Position_Software_Engineer": "",
+   "Position_Software_Client": "https://docs.google.com/forms/d/e/1FAIpQLSfWrQlG1ewhGAtKYrVyLVmTQ4KPdm99j9lwm049UywL-yhL9w/viewform?usp=sf_link"
 
 };
 
