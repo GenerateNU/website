e6ff9617cd4c29f7709658e871d926265ebd0568
Alix Heudebourg <alix.heudebourg@gmail.com>
2023-07-11 20:58:13 -0400

    priving a point

--- src/services/constants.js
+++ src/services/constants.js
@@ -32,7 +32,11 @@ export const constants = {
 
    // HW
    "Position_Hardware_Client": "https://docs.google.com/forms/d/e/1FAIpQLSc7TVI0FmZG6DujNi2GLEUeyGBQYlhAGtFUyf5AXzLIB0mF9Q/viewform?usp=sf_link",
-   "Position_Software_Client": "https://docs.google.com/forms/d/e/1FAIpQLSfWrQlG1ewhGAtKYrVyLVmTQ4KPdm99j9lwm049UywL-yhL9w/viewform?usp=sf_link"
+   "Position_Software_Client": "https://docs.google.com/forms/d/e/1FAIpQLSfWrQlG1ewhGAtKYrVyLVmTQ4KPdm99j9lwm049UywL-yhL9w/viewform?usp=sf_link",
+
+   //Sw
+
+   "lergkg": "",
 
 };
 
