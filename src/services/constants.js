cf7118b86365e83a780e6b4fa613dc06fe5125be
Alix Heudebourg <alix.heudebourg@gmail.com>
2023-07-23 21:42:33 -0400

    adding constants links for operations, engagement and software member apps

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,44 +1,50 @@
 export const constants = {
    //Management
-   // "Position_Executive_Director": "https://forms.gle/SFo5x49Xz8hcFDZj6",
-   "Position_Hardware_Director": "https://forms.gle/2A6tV8Xe2bp8q1TNA",
-   "Position_Software_Director": "https://forms.gle/tegB5SWsEkqe5YW17",
-   "Position_Operations_Director": "https://forms.gle/8jkatNqUu7VB88he8",
-   "Position_Engagement_Director": "https://forms.gle/vQXUHNKxhQkDpkns6",
+   // // "Position_Executive_Director": "https://forms.gle/SFo5x49Xz8hcFDZj6",
+   // "Position_Hardware_Director": "https://forms.gle/2A6tV8Xe2bp8q1TNA",
+   // "Position_Software_Director": "https://forms.gle/tegB5SWsEkqe5YW17",
+   // "Position_Operations_Director": "https://forms.gle/8jkatNqUu7VB88he8",
+   // "Position_Engagement_Director": "https://forms.gle/vQXUHNKxhQkDpkns6",
 
-   // Engagement
-   "Position_Events_Lead": "https://forms.gle/4xGHGtk87uybeTsKA",
-   "Position_Content_Lead": "https://forms.gle/wiRcTtHoQnZhZ3jW7",
-   "Position_Relations_Lead": "https://forms.gle/eD4uBFKvPkw2PCK68",
+   // // Engagement
+   // "Position_Events_Lead": "https://forms.gle/4xGHGtk87uybeTsKA",
+   // "Position_Content_Lead": "https://forms.gle/wiRcTtHoQnZhZ3jW7",
+   // "Position_Relations_Lead": "https://forms.gle/eD4uBFKvPkw2PCK68",
+   'Position_Event_Coordinator': 'https://docs.google.com/forms/d/e/1FAIpQLScEgtCYc16abSIQYn2-NGfdjFIlXA-2IEJKz0MT0brDMlz_uA/viewform?usp=sf_link',
+   'Position_Recruitment_Specialist': 'https://docs.google.com/forms/d/e/1FAIpQLScMiJ5Ux0JggzpDtZCuapiyyAllUCZU7-GXQTv8Kxz7E_MFVw/viewform?usp=sf_link',
+   'Position_Brand_Designer': 'https://docs.google.com/forms/d/e/1FAIpQLSdRu_kaL4nrOmqZgB57PEfJldOa6qSrd6ti3tIi8X-V9rNA4w/viewform?usp=sf_link',
+   'Position_Social_Media_Manager': 'https://docs.google.com/forms/d/e/1FAIpQLSf8_FpLwYuZa5ib6pMUKjMVTPLU_syAZhDbQkrBBUvTzs5qsw/viewform?usp=sf_link',
+   'Position_Copy_Writer': 'https://docs.google.com/forms/d/e/1FAIpQLSemYVMhWVtTKSpGkM-CunyRrAszWmfjr50ziK5Gk0AbRRQQKA/viewform?usp=sf_link',
+   'Position_Photographer/Videographer': 'https://docs.google.com/forms/d/e/1FAIpQLSeqvm1MZIXdx1R-I0Q2tu_mo7xEnOWe1oPBZ9M-hjTlzHZRGw/viewform?usp=sf_link',
 
 
    // Operations
-   "Position_Data_&_Strategy_Lead": "https://forms.gle/UMTCmjeHmsF1q9av8",
-   "Position_Operational_Support_Lead": "https://forms.gle/rVd7jupMAFfo7vHQ8",
-   "Position_Organizational_Development_Lead": "https://forms.gle/sWNnjZm3N1wYGWyz6",
+   // "Position_Data_&_Strategy_Lead": "https://forms.gle/UMTCmjeHmsF1q9av8",
+   // "Position_Operational_Support_Lead": "https://forms.gle/rVd7jupMAFfo7vHQ8",
+   // "Position_Organizational_Development_Lead": "https://forms.gle/sWNnjZm3N1wYGWyz6",
+   'Position_Data_Engineer': 'https://docs.google.com/forms/d/e/1FAIpQLSeB06dEz1zjW6eoulEySZCCsYyswXwoPoCwU1hTd9WAdeDgaQ/viewform?usp=sf_link',
+   'Position_Business_Analyst': 'https://docs.google.com/forms/d/e/1FAIpQLSdLc1B9GIT0Oh73bgQls2-ygF_VOWXeYICcJsuZd_8uI93ueQ/viewform?usp=sf_link',
+   'Position_Tech_Specialist': 'https://docs.google.com/forms/d/e/1FAIpQLSdDyIOdbY5B3prEaGy1V79OVVzkT7FGMcmoXHP8NUYaiNpD9A/viewform?usp=sf_link',
+   'Position_Archive_Analyst': 'https://docs.google.com/forms/d/e/1FAIpQLScUeqTYY1TLV5Ngz5veKJQ1lWv-XsspeyvJGuNfyZhOLGwTWQ/viewform?usp=sf_link',
+   'Position_Financial_Analyst': 'https://docs.google.com/forms/d/e/1FAIpQLSd9TfZzHvS_mLgZUAZHorjmuMlCe4t8O3DTfMV3893xoxqq-g/viewform?usp=sf_link',
+   'Position_Member_Development_Specialist': 'https://docs.google.com/forms/d/e/1FAIpQLSdG4QnjWET-ZyeDZRqWfZW0dscHjbBdu3OtsX4lEOEahO9DZw/viewform?usp=sf_link',
+   'Position_Corporate_Partnership_Coordinator': 'https://docs.google.com/forms/d/e/1FAIpQLSe1pp4Spj9tmwLrc_NLoSTPqtuLM7HKmRmMCK5oedqtEGcgKA/viewform?usp=sf_link',
 
-
-   // "Position_Office_Coordinator": "https://forms.gle/VMTLj1o3NhcDLsuU6",
-   // "Position_Data_Analyst": "https://forms.gle/48qdD8n52wBDRVT77",
-   // "Position_Strategic_Partnerships_Coordinator":
-   //    "https://forms.gle/63KhXsRpJXQWFtES6",
-   // "Position_Member_Development_Specialist": "https://forms.gle/6xujDs9yBNLofZ5YA",
-   // "Position_Employer_Events_Coordinator": "https://forms.gle/mCFM6DvVg6YQkuHHA",
-   // "Position_Capital_Development_Coordinator":
-   //    "https://forms.gle/8VZGCwZ1Fv5Z8rSDA",
-   // "Position_Tech_Specialist": "https://forms.gle/8iLm6tFzC5YVxYo5A",
-   // "Position_Makerspace_Specialist": "https://forms.gle/zRz2tBdwpQRhjsua6",
-   // "Position_Information_Architect/Copywriter": "https://forms.gle/CmNQCADrwxAMuo8aA",
 
    // HW
-   "Position_Hardware_Client": "https://docs.google.com/forms/d/e/1FAIpQLSc7TVI0FmZG6DujNi2GLEUeyGBQYlhAGtFUyf5AXzLIB0mF9Q/viewform?usp=sf_link",
-   "Position_Software_Client": "https://docs.google.com/forms/d/e/1FAIpQLSfWrQlG1ewhGAtKYrVyLVmTQ4KPdm99j9lwm049UywL-yhL9w/viewform?usp=sf_link",
-   "Position_Lead_Engineer": "https://forms.gle/fVc1cmJQCWCU6CDn9",
+   // "Position_Hardware_Client": "https://docs.google.com/forms/d/e/1FAIpQLSc7TVI0FmZG6DujNi2GLEUeyGBQYlhAGtFUyf5AXzLIB0mF9Q/viewform?usp=sf_link",
+   // "Position_Software_Client": "https://docs.google.com/forms/d/e/1FAIpQLSfWrQlG1ewhGAtKYrVyLVmTQ4KPdm99j9lwm049UywL-yhL9w/viewform?usp=sf_link",
+   // "Position_Lead_Engineer": "https://forms.gle/fVc1cmJQCWCU6CDn9",
+   'Position_Hardware_Engineer': '',
+   'Position_Industrial_Designer': '',
 
    //Sw
 
-   "Position_Tech_Lead": "https://forms.gle/HTZMjFHDBWK9UWz89",
-   "Position_Design_Lead": "https://forms.gle/7KxTtPbYKf7HWBJy7",
+   // "Position_Tech_Lead": "https://forms.gle/HTZMjFHDBWK9UWz89",
+   // "Position_Design_Lead": "https://forms.gle/7KxTtPbYKf7HWBJy7",
+   "Postition_Software_Engineer": "https://forms.gle/uxFgicwwkPaejbqu9",
+   "Postition_Software_Designer": "https://forms.gle/8P16ncygCyVSoUBx6",
+
 
 };
 
