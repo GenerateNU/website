36c42198666d6a660582bcb4b7e3d4fb2995adf0
leoRysing <leshaigor@gmail.com>
2023-09-01 11:50:30 -0400

    Mobile footer - all touched up

--- src/component/HorizontalFooter/index.js
+++ src/component/HorizontalFooter/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./style.css";
-import GenerateLogo from "../../assets/images/landingpage-v2/footerlogo.svg"  ;//"../../assets/images/landingpage-v2/footerlogo.svg";
+import GenerateLogo from "../../assets/images/landingpage-v2/footerlogo.svg"; //"../../assets/images/landingpage-v2/footerlogo.svg";
 import InstaIcon from "../../assets/images/socialMediaIcons/Insta.png";
 import LinkedInIcon from "../../assets/images/socialMediaIcons/Linkedin.png";
 import FbIcon from "../../assets/images/socialMediaIcons/Facebook.png";
@@ -11,55 +11,79 @@ import NortheasternIcon from "../../assets/images/socialMediaIcons/Northeaster.p
 import Sherm from "../../assets/images/socialMediaIcons/Sherm.png";
 import { useNavigate } from "react-router-dom";
 
+const pages = [
+  { name: "Generate", link: "/" },
+  { name: "Apply", link: "/apply" },
+  { name: "About", link: "/about" },
+  { name: "Culture", link: "/culture" },
+  { name: "Teams", link: "/teams" },
+  // { name: "People", link: "/", disabled: true },
+  { name: "Projects", link: "/projects" },
+];
+
+const FooterLink = ({ page, currentPage }) => {
+  const { name, link, disabled } = page;
+  const isCurrentPage = currentPage === link;
+
+  return (
+    <div>
+      {disabled ? (
+        <a className="footer-link-text disabled-footer-text" href={link}>
+          {name}
+        </a>
+      ) : isCurrentPage ? (
+        <b>
+          <a href={link} className="footer-link-bold">
+            {name} {"<"}
+          </a>
+        </b>
+      ) : (
+        <a className="footer-link-text" href={link}>
+          {name}
+        </a>
+      )}
+    </div>
+  );
+};
+
 function HorizontalFooter() {
   const navigate = useNavigate();
+  const currentPageUrl = window.location.href;
   const handleOnClick = () => {
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
     navigate("/");
   };
+
   return (
-    <div className="footer-container">
-      <Row>
-        <div className="w-100">
-          <div className="sherm d-flex justify-content-center align">
-              <img            
+    <Container className="footer-container">
+      <div className="d-flex flex-row justify-content-between w-100">
+        <div className="left-bar">
+          <div className="w-100 d-flex flex-column justify-content-between">
+            <div className="sherm">
+              <img
                 className="logo-placement"
                 src={GenerateLogo}
                 onClick={handleOnClick}
                 alt="Logo description" // Add alt text to the image for accessibility
               />
+            </div>
           </div>
-        </div>
-      </Row>
-      <div className="d-flex flex-row justify-content-between">
-        <div>
-          <Row className="footer-text">
-            {" "}
-            <a href="/about"> About </a>{" "}
-          </Row>{" "}
-          <Row className="footer-text">
-            {" "}
-            <a href="/culture"> Culture </a>{" "}
-          </Row>{" "}
-          <Row className="footer-text">
-            {" "}
-            <a href="/"> Teams </a>{" "}
-          </Row>{" "}
-          <Row className="footer-text">
-            {" "}
-            <a href="/"> People </a>{" "}
-          </Row>{" "}
-          <Row className="footer-text">
-            {" "}
-            <a href="/apply"> Apply </a>{" "}
-          </Row>{" "}
-          <Row className="footer-text">
-            {" "}
-            <a href="/"> Projects </a>{" "}
-          </Row>{" "}
+          <span id="footer-links">
+            <span id="footer-pages">
+              {pages.map((page, index) => (
+                <FooterLink
+                  key={index}
+                  page={page}
+                  currentPage={currentPageUrl.substring(
+                    currentPageUrl.lastIndexOf("/")
+                  )}
+                />
+              ))}
+            </span>
+          </span>
         </div>
         <div className="social-icons">
-          <Row>
+          <Row style={{ paddingBottom: 0 }}>
             <Col className="icon">
               <a href="https://www.google.com/">
                 {" "}
@@ -67,7 +91,7 @@ function HorizontalFooter() {
               </a>{" "}
             </Col>{" "}
           </Row>{" "}
-          <Row>
+          <Row style={{ paddingBottom: 0 }}>
             <Col className="icon">
               <a href="https://www.youtube.com/channel/UC2Y_rgZiPKPH0lSU1FyQJFw/featured">
                 {" "}
@@ -81,7 +105,7 @@ function HorizontalFooter() {
               </a>{" "}
             </Col>{" "}
           </Row>{" "}
-          <Row>
+          <Row style={{ paddingBottom: 0 }}>
             <Col className="icon">
               <a href="https://www.linkedin.com/company/generate-product-development/mycompany/">
                 {" "}
@@ -95,7 +119,7 @@ function HorizontalFooter() {
               </a>{" "}
             </Col>{" "}
           </Row>{" "}
-          <Row>
+          <Row style={{ paddingBottom: 0 }}>
             <Col className="icon">
               <a href="https://www.linkedin.com/company/generate-product-development/mycompany/">
                 {" "}
@@ -111,7 +135,7 @@ function HorizontalFooter() {
           </Row>{" "}
         </div>{" "}
       </div>{" "}
-    </div>
+    </Container>
   );
 }
 
--- src/shared/useMobile.js
+++ src/shared/useMobile.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 export default function useWebsite() {
     const isBigScreen = !window.matchMedia("max-device-width: 650px").matches;
-    console.log("is it a big screen;", isBigScreen, " : ", console.log(window.innerWidth));
-    const [isWebsite, setIsWebsite] = useState(isBigScreen);
+    console.log("is it a big screen;", isBigScreen, " : ", window.innerWidth);
+    const [isWebsite, setIsWebsite] = useState(window.innerWidth > 650);
     //choose the screen size
     const handleResize = () => {
         if (window.innerWidth > 650) {
