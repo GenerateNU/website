f2dbee425ae029d1e54a45441653560c3df79a31
davebudhram <davebudhram18@gmail.com>
2023-09-20 21:29:50 -0400

    finished about page fixes

--- src/component/AboutPage/Leadership/index.jsx
+++ src/component/AboutPage/Leadership/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/esm/Row";
 import directorData from "../../../data/directorData";
 import LeadershipCard from "./leadershipCard";
 
@@ -9,12 +8,13 @@ export default function Leadership() {
     <>
       <Container
         fluid
-        className="dsktop bg-black vh-100 vw-100 position-relative"
-      >
-        <Row className="vh-25 p-5">
+        className="dsktop bg-black vh-100 vw-100 position-relative ">
+         
+          <div className="leadership-header vh-25 ">
           <h3 className="leadership-heading">Leadership</h3>
           <h2 className="meet-the-team">Meet the team</h2>
-        </Row>
+          </div>
+       
         <div className="vh-75 p-5 d-flex cards-display">
           {directorData
             .map((dir) => {
@@ -23,22 +23,25 @@ export default function Leadership() {
             .reverse()}
         </div>
       </Container>
+
       <Container
         fluid
         className="mob bg-black container-h vw-100 position-relative"
       >
-        <Row className="vh-15 p-5">
+        <div className=" leadership-header no-top-padding ">
           <h3 className="leadership-heading">Leadership</h3>
-          <h2 className="meet-the-team">Meet the team</h2>
-        </Row>
-        <div className="vh-75 p-5 d-flex cards-display">
+          <h2 className="meet-the-team text-no-wrap">Meet the team</h2>
+        </div>
+      <div className="vh-75 p-5 d-flex cards-display">
           {directorData
             .map((dir) => {
               return <LeadershipCard director={dir} key={dir.title} />;
             })
             .reverse()}
         </div>
-      </Container>
+        </Container>
+
+      
     </>
   );
 }
