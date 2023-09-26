c409b9d8568f67de2e828a918d82e5f63b20bb90
Derrick Kim <kim.der@northeastern.edu>
2023-09-26 16:37:52 -0400

    add mobile pages

--- src/component/CulturePage/IntroImages/index.jsx
+++ src/component/CulturePage/IntroImages/index.jsx
@@ -4,7 +4,7 @@ import IntroImg1 from "../../../component/CulturePage/IntroImages/website-2.jpg"
 import IntroImg2 from "../../../component/CulturePage/IntroImages/website-3.jpg";
 import IntroImg3 from "../../../component/CulturePage/IntroImages/website-4.jpg";
 
-export default function IntroImages() {
+const desktopContent = () => {
   return (
     <div className="intro-images">
       <div className="for-padding">
@@ -18,4 +18,20 @@ export default function IntroImages() {
       </div>
     </div>
   );
+}
+
+const mobileContent = () => {
+  return (
+    <div className="intro-images-container-mobile">
+      <text className="fun-text-mobile"> HAVE SOME FUN WITH US </text>
+      <img className="img-formatting-mobile" src={IntroImg1} alt="IntroImage1" />
+      <img className="img-formatting-mobile" src={IntroImg2} alt="IntroImage2" />
+      <img className="img-formatting-mobile" src={IntroImg3} alt="IntroImage3" />
+      <div className="line-mobile" />
+    </div>
+  );
+}
+
+export default function IntroSection(props) {
+  return props.disp ? mobileContent() : desktopContent()
 }
--- src/component/CulturePage/IntroSection/index.jsx
+++ src/component/CulturePage/IntroSection/index.jsx
@@ -3,28 +3,43 @@ import NavBar from "../../NavBar";
 import logo from "../IntroSection/GeneratePeaceLogo.png";
 import "./style.css";
 
-export default function IntroSection() {
-  return (
-    <div className="position-relative intro-container">
-      <div className="intro-navbar">
-        <NavBar />
-      </div>
+const desktopContent = () => (
+  <div className="position-relative intro-container">
+    <div className="intro-navbar">
+      <NavBar />
+    </div>
 
-      <div className="d-flex">
-        <div className="generateMascotContainer">
-          <img className="generateMascot" src={logo} alt="Generate Mascot" />
+    <div className="d-flex">
+      <div className="generateMascotContainer">
+        <img className="generateMascot" src={logo} alt="Generate Mascot" />
+      </div>
+      <div className="our-culture-quote-positioning p-3">
+        <div className="our-culture-quote">
+          We make wicked projects &amp; plan wicked events
         </div>
-        <div className="our-culture-quote-positioning p-3">
-          <div className="our-culture-quote">
-            We make wicked projects &amp; plan wicked events
-          </div>
-        </div>
-        <div className="our-culture-big-positioning">
-          <div className="our-culture-big text-no-fill text-uppercase position-relative start-0 bottom-0-culture p-5">
-            Our Culture
-          </div>
+      </div>
+      <div className="our-culture-big-positioning">
+        <div className="our-culture-big text-no-fill text-uppercase position-relative start-0 bottom-0-culture p-5">
+          Our Culture
         </div>
       </div>
     </div>
-  );
+  </div>
+);
+
+const mobileContent = () => (
+  <>
+    <div className="intro-navbar">
+      <NavBar />
+    </div>
+    <div className="intro-container-mobile">
+      <text className="our-culture-big-mobile text-no-fill"> OUR CULTURE </text>
+      <text className="our-culture-quote-mobile"> We make wicked projects &amp; plan wicked events </text>
+      <img className="generateMascot-mobile" src={logo} alt="Generate Mascot" />
+    </div>
+  </>
+)
+
+export default function IntroSection(props) {
+  return props.disp ? mobileContent() : desktopContent()
 }
