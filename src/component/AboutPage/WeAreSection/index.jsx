cb64e7724f2ac51d927ba776635d77757ad029be
davebudhram <davebudhram18@gmail.com>
2023-10-05 19:05:35 -0400

    finished we are section mobile page

--- src/component/AboutPage/WeAreSection/index.jsx
+++ src/component/AboutPage/WeAreSection/index.jsx
@@ -1,25 +1,28 @@
-import React from "react";
-import ValueCard from "./valueCard";
-import valueData from "../../../data/valueData";
-import './style.css';
+import React from 'react'
+import ValueCard from './valueCard'
+import valueData from '../../../data/valueData'
+import './style.css'
 
 const WeAreSection = () => {
-    return (
-        <div className="we-are-our-values-container"> 
-                <div className="weare-header">
-                    <h5 className="mb-0 our-mission-our-values we-are-values d-flex flex-row justify-content-end h-auto text-nowrap">
-                        our values
-                    </h5>
-                    <h1 className="our-mission-we-are text-no-fill text-uppercase text-nowrap">
-                        We are
-                    </h1>
-                    </div>
-            <div className="outside-card-container">{
-                valueData.map(value => <div className="card-container"><ValueCard value={value}/></div>)
-            }</div>
-         </div>
-           
-    );
-};
+  return (
+    <div className='we-are-our-values-container'>
+      <div className='weare-header'>
+        <h5 className='mb-0 our-mission-our-values we-are-values d-flex flex-row justify-content-end h-auto text-nowrap'>
+          our values
+        </h5>
+        <h1 className='our-mission-we-are text-no-fill text-uppercase text-nowrap'>
+          We are
+        </h1>
+      </div>
+      <div className='outside-card-container'>
+        {valueData.map((value) => (
+          <div className='card-container'>
+            <ValueCard value={value} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
-export default WeAreSection; 
+export default WeAreSection
