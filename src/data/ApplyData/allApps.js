096aaee4f31ff4f314d33fc2036f9034f15392e3
leoRysing <leshaigor@gmail.com>
2023-10-08 18:44:55 -0400

    json level changes to make this work

--- src/data/ApplyData/allApps.js
+++ src/data/ApplyData/allApps.js
@@ -1,7 +1,12 @@
 import Clients from "./clients";
 
 const applicationsByTeams = {
-    clients: Clients
+    clients: Clients,
+    hardware: [],
+    software: [],
+    operations: [],
+    managment: [],
+    engagement: []
 }
 
 export default applicationsByTeams;
