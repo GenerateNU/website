b2590b75d71ae494a528bfe60f624c5f5178768e
haleymartin-6 <119809061+haleymartin-6@users.noreply.github.com>
2023-10-17 16:55:04 -0400

    fixed mobile resize on intro page and fixed up fp1 & fp2 to be based on vw

--- src/component/LandingPageV2/FP2/index.jsx
+++ src/component/LandingPageV2/FP2/index.jsx
@@ -1,118 +1,117 @@
-import React from "react";
-import "./style.css";
-import BottomCorner from "../../../assets/images/landingpage-v2/bottom_corner.png";
-import TopCorner from "../../../assets/images/landingpage-v2/top_corner.png";
-import useDeviceDetection from "../../DesktopMobileHandler/isMobileDetection.hooks.jsx";
-import Projects from "../../../data/featuredProjectData.js";
-import useWebsite from "../../../shared/useWebsite";
-
+import React from 'react'
+import './style.css'
+import BottomCorner from '../../../assets/images/landingpage-v2/bottom_corner.png'
+import TopCorner from '../../../assets/images/landingpage-v2/top_corner.png'
+import useDeviceDetection from '../../DesktopMobileHandler/isMobileDetection.hooks.jsx'
+import Projects from '../../../data/featuredProjectData.js'
+import useWebsite from '../../../shared/useWebsite'
 
 export default function FP2() {
   const isWebsite = useWebsite()
   const isBigScreen = !window.matchMedia('(max-device-width: 650px)').matches
   const mobile = !isBigScreen || !isWebsite
-  const SmartyPill = Projects[1];
+  const SmartyPill = Projects[1]
 
   if (mobile) {
     return (
-      <div className="parent">
+      <div className='parent'>
         <div>
-          <div className="heading-container">
-            <h1 id="fp2-header">{SmartyPill.title}</h1>
-            <p id="fp2-teams">{SmartyPill.type}</p>
+          <div className='heading-container'>
+            <h1 id='fp2-header'>{SmartyPill.name}</h1>
+            <p id='fp2-teams'>{SmartyPill.type}</p>
           </div>
-          <div className="client">
-            <p className="client-text">Client</p>
-            <div className="name-order">
-              <p style={{ fontSize: "3vh" }} className="name-text">
+          <div className='client'>
+            <p className='client-text'>Client</p>
+            <div className='name-order'>
+              <p style={{ fontSize: '3vh' }} className='name-text'>
                 {SmartyPill.clientName}
               </p>
-              <p style={{ fontSize: "2vh" }} className="school">
+              <p style={{ fontSize: '2vh' }} className='school'>
                 {SmartyPill.clientSchool}
               </p>
             </div>
           </div>
-          <div className="client-blurb" id="fp2-client-blurb">
-            <p style={{ width: "40vh" }}>{SmartyPill.description}</p>
-            <a href="url">learn more</a>
+          <div className='client-blurb' id='fp2-client-blurb'>
+            <p style={{ width: '40vh' }}>{SmartyPill.description}</p>
+            <a href='url'>learn more</a>
           </div>
-          <div className="bottom-corner">
-            <img style={{ height: "25vh" }} src={BottomCorner} alt="" />
-          </div>
-        </div>
-        <div>
-          <div className="product-img-container">
-            <img className="mockup-img" src={SmartyPill.image} alt="" />
+          <div className='bottom-corner'>
+            <img style={{ height: '25vh' }} src={BottomCorner} alt='' />
           </div>
         </div>
         <div>
-          <div className="top-corner">
+          <div className='product-img-container'>
+            <img className='mockup-img' src={SmartyPill.image} alt='' />
+          </div>
+        </div>
+        <div>
+          <div className='top-corner'>
             <img
-              style={{ height: "25vh" }}
+              style={{ height: '25vh' }}
               src={TopCorner}
-              alt="Smarty_Pill"
-              width="45%"
+              alt='Smarty_Pill'
+              width='45%'
             />
           </div>
-          <div className="quote-container">
+          <div className='quote-container'>
             <blockquote>{SmartyPill.clientQuote}</blockquote>
           </div>
         </div>
       </div>
-    );
+    )
   } else {
     return (
       <>
-        <div className="column-1">
-          <div className="column-1-text">
+        <div className='column-1'>
+          <div className='column-1-text'>
             <div>
-              <h1 style={{ fontSize: "9vh" }}>{SmartyPill.name}</h1>
-              <p style={{ fontFamily: "Space Mono", fontSize: "3vh" }}>
+              <h1 style={{ fontSize: '9vh' }}>{SmartyPill.name}</h1>
+              <p style={{ fontFamily: 'Space Mono', fontSize: '3vh' }}>
                 {SmartyPill.type}
               </p>
             </div>
-            <div className="client">
-              <p className="client-text">Client</p>
-              <div className="name-order">
-                <p style={{ fontSize: "3vh" }} className="name-text">
+            <div className='client'>
+              <p className='client-text'>Client</p>
+              <div className='name-order'>
+                <p style={{ fontSize: '3vh' }} className='name-text'>
                   {SmartyPill.clientName}
                 </p>
-                <p style={{ fontSize: "2vh" }} className="school">
+                <p style={{ fontSize: '2vh' }} className='school'>
                   {SmartyPill.clientSchool}
                 </p>
               </div>
             </div>
             <div>
-              <p style={{ width: "40vh" }} className="client-blurb">
+              <p style={{ width: '40vh' }} className='client-blurb'>
                 {SmartyPill.description}
               </p>
-              <a href="url">learn more</a>
+              <a href='url'>learn more</a>
             </div>
           </div>
-          <div className="bottom-corner">
-            <img className="bottom-corner-img" src={BottomCorner} alt="" />
+          <div className='bottom-corner'>
+            <img className='bottom-corner-img' src={BottomCorner} alt='' />
           </div>
         </div>
-        <div className="column-2">
-          <div className="product-img-container">
-            <img className="mockup-img" src={SmartyPill.image} alt="" />
+        <div className='column-2'>
+          <div className='product-img-container'>
+            <img className='mockup-img' src={SmartyPill.image} alt='' />
           </div>
         </div>
-        <div className="column-3">
-          <div className="top-corner">
+        <div className='column-3'>
+          <div className='top-corner'>
             <img
-              className="top-corner-img"
+              className='top-corner-img'
               src={TopCorner}
-              alt="Smarty_Pill"
-              width="45%"
+              alt='Smarty_Pill'
+              width='45%'
             />
           </div>
-          <blockquote className="block_quote">
-            <div className="quote-container">{SmartyPill.clientQuote}</div>
+          <blockquote className='block_quote'>
+            <div className='quote-container'>{SmartyPill.clientQuote}</div>
           </blockquote>
         </div>
       </>
-    );
+    )
   }
 }
 
