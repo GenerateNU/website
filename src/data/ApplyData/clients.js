7bfb96278f491ad1b3776c85d3067f1dbc62c44c
leoRysing <leshaigor@gmail.com>
2023-10-18 22:25:45 -0400

    Client json changes

--- src/data/ApplyData/clients.js
+++ src/data/ApplyData/clients.js
@@ -1,41 +1,30 @@
-const Clients = [
-  {
-    positionTitle: 'Client',
-    categoryType: 'Clients',
-    description:
-      "Have an idea for a product but don't have the technical background to build it? Have an existing product that you want new features added to? Apply to be a Generate client! We work semesterly on a wide variety of Hardware & Software projects that meet our client's needs, to help them advance to the next stage of their venture's development. Don't know if your project is a good fit? Still apply and we'll work with you to determine feasibility and capability!",
-    responsibilities:
-      'Work with your Project Lead (PL) on crafting a scope that best fits your goals and the capabilities of our team. Meet with your PL (and occasionally your entire team) regularly. Sit back, relax, and allow us to build your product and advance your venture!',
-    requirements:
-      'The client has retained Generate for the purpose of designing, consulting, and other related services for a project',
-    startDate: '1/8/2024',
-    endDate: '4/12/2024',
-    workCommitment: 0,
-    active: true,
-    remarks:
-      'The position begins and ends with the Northeastern academic semester. The last day to apply is November 1st.',
-    applicationLink:
-      'https://docs.google.com/forms/d/e/1FAIpQLSd8PPVmILgy92GdVZCn8cYm1sSgsnKZVxnaNDxA8KKL1JxYSg/viewform?usp=sharing'
-  }
-  // this is a bogus app for testing purposes, delete later
-  // {
-  //   positionTitle: 'Client Two',
-  //   categoryType: 'Clients',
-  //   description:
-  //     "If you're seeking innovative solutions for your business, project, or startup, apply to have Generate turn your ideas into reality.",
-  //   responsibilities:
-  //     'I agree to respond to official communications within 2 business days, except in emergencies, where I will inform my project lead of the situation. I agree to attend Generate meetings and orientations, except in emergencies, where I will inform the host of the meeting I cannot attend. I understand that my official relationship with Generate lasts for only one semester and that work done by the Generate Team will not continue past one semester. I understand that if I am interested in having a member of Generate continue to work on my project as a freelancer I will reach out to my Project lead to discuss this and will not contact the individual directly first. I understand that my relationship with Generate is at-will, and that the Management Team reserves the right to terminate that relationship at any time. I understand that not adhering to this contract may result in loss of access to the Sherman Center and termination of my relationship with Generate. If I am working with a Hardware team, I understand that development on my project may be continued without me at the discretion of the project lead. The completed work at the end of the semester will remain with Generate and the Sherman Center and will not be given to any individual, including myself. If I am working with a Software team, I understand that development on my project may be continued without me at the discretion of the project lead. Any completed work, whether completed before or after the time of termination, will remain with Generate and the Sherman Center and will not be given to any individual, including myself.',
-  //   requirements:
-  //     'The client has retained Generate for the purpose of designing, consulting, and other related services for a project',
-  //   startDate: '1/8/2024',
-  //   endDate: '4/12/2024',
-  //   workCommitment: 0,
-  //   active: false,
-  //   remarks:
-  //     'The position begins and ends with the Northeastern academic semester. The last day to apply is November 1st.',
-  //   applicationLink:
-  //     'https://docs.google.com/forms/d/e/1FAIpQLSd8PPVmILgy92GdVZCn8cYm1sSgsnKZVxnaNDxA8KKL1JxYSg/viewform?usp=sharing'
-  // }
+const Clients = [{
+        positionTitle: 'Client',
+        categoryType: 'Clients',
+        description: "Have an idea for a product but don't have the technical background to build it? Have an existing product that you want new features added to? Apply to be a Generate client! We work semesterly on a wide variety of Hardware & Software projects that meet our client's needs, to help them advance to the next stage of their venture's development. Don't know if your project is a good fit? Still apply and we'll work with you to determine feasibility and capability!",
+        responsibilities: 'Work with your Project Lead (PL) on crafting a scope that best fits your goals and the capabilities of our team. Meet with your PL (and occasionally your entire team) regularly. Sit back, relax, and allow us to build your product and advance your venture!',
+        requirements: 'The client has retained Generate for the purpose of designing, consulting, and other related services for a project',
+        startDate: '1/8/2024',
+        endDate: '4/19/2024',
+        workCommitment: 5,
+        active: true,
+        remarks: 'The position begins and ends with the Northeastern academic semester. The last day to apply is November 10th.',
+        applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSd8PPVmILgy92GdVZCn8cYm1sSgsnKZVxnaNDxA8KKL1JxYSg/viewform?usp=sharing'
+    },
+    //this is a bogus app for testing purposes, delete later
+    // {
+    //     positionTitle: 'Client Two',
+    //     categoryType: 'Clients',
+    //     description: "If you're seeking innovative solutions for your business, project, or startup, apply to have Generate turn your ideas into reality.",
+    //     responsibilities: 'I agree to respond to official communications within 2 business days, except in emergencies, where I will inform my project lead of the situation. I agree to attend Generate meetings and orientations, except in emergencies, where I will inform the host of the meeting I cannot attend. I understand that my official relationship with Generate lasts for only one semester and that work done by the Generate Team will not continue past one semester. I understand that if I am interested in having a member of Generate continue to work on my project as a freelancer I will reach out to my Project lead to discuss this and will not contact the individual directly first. I understand that my relationship with Generate is at-will, and that the Management Team reserves the right to terminate that relationship at any time. I understand that not adhering to this contract may result in loss of access to the Sherman Center and termination of my relationship with Generate. If I am working with a Hardware team, I understand that development on my project may be continued without me at the discretion of the project lead. The completed work at the end of the semester will remain with Generate and the Sherman Center and will not be given to any individual, including myself. If I am working with a Software team, I understand that development on my project may be continued without me at the discretion of the project lead. Any completed work, whether completed before or after the time of termination, will remain with Generate and the Sherman Center and will not be given to any individual, including myself.',
+    //     requirements: 'The client has retained Generate for the purpose of designing, consulting, and other related services for a project',
+    //     startDate: '1/8/2024',
+    //     endDate: '4/19/2024',
+    //     workCommitment: 5,
+    //     active: false,
+    //     remarks: 'The position begins and ends with the Northeastern academic semester. The last day to apply is November 10th.',
+    //     applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSd8PPVmILgy92GdVZCn8cYm1sSgsnKZVxnaNDxA8KKL1JxYSg/viewform?usp=sharing'
+    // }
 ]
 
 export default Clients
