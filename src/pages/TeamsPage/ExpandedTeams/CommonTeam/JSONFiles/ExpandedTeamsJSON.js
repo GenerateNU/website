0cecfaed7400318212a45e09295edfaced4d17df
Dave Budhram <davebudhram18@gmail.com>
2023-10-26 15:06:37 -0400

    adding op and eng pics

--- src/pages/TeamsPage/ExpandedTeams/CommonTeam/JSONFiles/ExpandedTeamsJSON.js
+++ src/pages/TeamsPage/ExpandedTeams/CommonTeam/JSONFiles/ExpandedTeamsJSON.js
@@ -4,8 +4,11 @@ import Legacy from '../../../../../assets/images/TeamsPageImages/legacy.jpg'
 import Management1 from '../../../../../assets/images/TeamsPageImages/management1.jpg'
 import Management2 from '../../../../../assets/images/TeamsPageImages/management2.jpg'
 import Operations1 from '../../../../../assets/images/TeamsPageImages/operations1.jpg'
+import Operations2 from '../../../../../assets/images/TeamsPageImages/operations2.jpg'
 import Hardware1 from '../../../../../assets/images/TeamsPageImages/hardware1.jpg'
 import Hardware2 from '../../../../../assets/images/TeamsPageImages/fallshowcase-43.jpg'
+import Engagement1 from '../../../../../assets/images/TeamsPageImages/engagement1.jpg'
+import Engagement2 from '../../../../../assets/images/TeamsPageImages/engagement2.jpg'
 // What Youll learn could probably be added here?
 export const ExpandedTeamsJSON = {
   management: {
@@ -37,7 +40,7 @@ export const ExpandedTeamsJSON = {
       {
         header: 'HOW WE WORK',
         body: 'Generate is entirely student-led. Our management team offers a unique opportunity for students to gain leadership experience in a fast-paced, innovative culture. The team is united through the yearly vision set by the executive director and builds initiatives around those goals. Each member leaves their own mark on Generate, continuously improving for the future.',
-        image: TeamsStock,
+        image: Operations2,
         invert: true
       }
     ]
@@ -121,12 +124,12 @@ export const ExpandedTeamsJSON = {
       {
         header: 'WHAT WE DO',
         body: "Our management team is the driving force behind Generate, empowering each member to grow and build innovative products. They work directly with members and clients to support their growth and the team's vision.They build our professional image and position the organization strategically within the Northeastern ecosystem, bridging our internal operations with the larger community.",
-        image: TeamsStock
+        image: Engagement1
       },
       {
         header: 'HOW WE WORK',
         body: 'Generate is entirely student-led. Our management team offers a unique opportunity for students to gain leadership experience in a fast-paced, innovative culture. The team is united through the yearly vision set by the executive director and builds initiatives around those goals. Each member leaves their own mark on Generate, continuously improving for the future.',
-        image: TeamsStock,
+        image: Engagement2,
         invert: true
       }
     ],
