aeaa29840cae369db6198456170c2c709f4ccb45
oliviaseds <osedarski@gmail.com>
2024-05-13 14:35:25 -0400

    audit packages, log basic query

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,5 +1,6 @@
 import role from './role'
 import director from './director'
+import pastProjects from './pastProject'
 import teamOverview from './teamOverview'
 import value from './value'
 import whyGenerate from './whyGenerate'
@@ -8,6 +9,7 @@ export const schemaTypes = [
   // Documents
   role,
   director,
+  pastProjects,
   teamOverview,
   value,
   whyGenerate,
--- src/App.js
+++ src/App.js
@@ -11,34 +11,133 @@ import ExpandedTeamsPage from './pages/TeamsPage/ExpandedTeams'
 import ProjectsPage from './component/ProjectsPage'
 import CaseStudy from './component/CaseStudyPage'
 
+import { client } from './client'
 /**
  *
  * The app.js the routes to the different pages are located.
  * @returns
  */
 export default function App() {
+  const [directors, setDirectors] = React.useState([])
+
+  React.useEffect(() => {
+    client.fetch(`*[_type == "director"]`).then((data) => {
+      setDirectors(data)
+    })
+  }, [])
+  console.log(directors)
+  
   return (
     <div className='App'>
       <Router>
         <div>
           <ScrollToTop />
           <Routes>
-            <Route path='/' element={<> <ScrollToTop /><LandingPageV2 /></>} />
-            <Route path='/apply' element={<> <ScrollToTop /> <ApplyPage /></>}>
-              <Route path=':team' element={<> <ScrollToTop /><ApplyPage /></>} />
+            <Route
+              path='/'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop />
+                  <LandingPageV2 />
+                </>
+              }
+            />
+            <Route
+              path='/apply'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <ApplyPage />
+                </>
+              }
+            >
+              <Route
+                path=':team'
+                element={
+                  <>
+                    {' '}
+                    <ScrollToTop />
+                    <ApplyPage />
+                  </>
+                }
+              />
             </Route>
-            <Route path='/positions/:id' element={<> <ScrollToTop /> <Position /></>} />
-            <Route path='/positions/:categoryType/:index' element={<> <ScrollToTop /> <Position /></>} />
-            <Route path='/about' element={<> <ScrollToTop /> <AboutPage /></>} />
-            <Route path='/culture' element={<> <ScrollToTop /> <CulturePage /></>} />
-            <Route path='/teams' element={<>    <ScrollToTop /><TeamsPage /></>} />
-            <Route path='/projects' element={<> <ScrollToTop /> <ProjectsPage /></>} />
-            <Route path='/case-study/:project' element={<> <ScrollToTop /> <CaseStudy /></>} />
-            <Route path='/teams-expanded/*' element={<> <ScrollToTop /> <ExpandedTeamsPage /></>} />
+            <Route
+              path='/positions/:id'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <Position />
+                </>
+              }
+            />
+            <Route
+              path='/positions/:categoryType/:index'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <Position />
+                </>
+              }
+            />
+            <Route
+              path='/about'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <AboutPage />
+                </>
+              }
+            />
+            <Route
+              path='/culture'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <CulturePage />
+                </>
+              }
+            />
+            <Route
+              path='/teams'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop />
+                  <TeamsPage />
+                </>
+              }
+            />
+            <Route
+              path='/projects'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <ProjectsPage />
+                </>
+              }
+            />
+            <Route
+              path='/case-study/:project'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <CaseStudy />
+                </>
+              }
+            />
+            <Route
+              path='/teams-expanded/*'
+              element={
+                <>
+                  {' '}
+                  <ScrollToTop /> <ExpandedTeamsPage />
+                </>
+              }
+            />
           </Routes>
         </div>
-
-
       </Router>
     </div>
   )
