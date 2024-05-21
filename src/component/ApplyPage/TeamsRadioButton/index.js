4796aa5d7f9d28b6b315204c0fcd257a324883dc
haleymartin-6 <119809061+haleymartin-6@users.noreply.github.com>
2024-05-21 13:08:50 -0400

    new application that works

--- src/component/ApplyPage/TeamsRadioButton/index.js
+++ src/component/ApplyPage/TeamsRadioButton/index.js
@@ -5,25 +5,25 @@ import { useNavigate } from 'react-router-dom'
 import {useSanity} from '../../../services/useSanity'
 
 export const TeamsRadioButton = ({ team, isSelected, onClick }) => {
+  const query = `*[_type == "application" && lower(team) == lower("${team}")]`
 
-  const query = `*[_type == "application" && team == ${team}] | order(zIndex desc)`
+  const rawApplications = useSanity(query, {}, (data) => data ? data.map((application) => ({
+    ...application,
+    startDate: new Date(application.startDate),
+    endDate: new Date(application.endDate),
+  })) : [])
 
-  const applications = useSanity(query, {}, (data) => data ? data.map((application) => ({
-      ...application,
-      startDate: new Date(application.startDate),
-      endDate: new Date(application.endDate),
-    })) : []
-  );
+  const applications = useMemo(() => findPositionsByCategory(team, rawApplications), [rawApplications, team])
+
 
   const navigate = useNavigate()
-
-  const positionsData = useMemo(() => findPositionsByCategory(team), [team])
 
   const handlePositionClick = (id, isActive) => {
     if (isActive) {
       navigate(`/positions/${team}/${id}`)
     }
   }
+
   return (
     <div className='teams-button-container'>
       <div
@@ -34,7 +34,7 @@ export const TeamsRadioButton = ({ team, isSelected, onClick }) => {
       </div>
       {isSelected && (
         <div className='positions-container'>
-          {positionsData.length === 0 ? (
+          {applications.length === 0 ? (
             <div className='no-positions-text'>No Positions Available</div>
           ) : (
             applications.map((position, index) => {
@@ -45,7 +45,6 @@ export const TeamsRadioButton = ({ team, isSelected, onClick }) => {
                     position.active ? 'position-text' : 'position-text-disabled'
                   }
                   onClick={() => handlePositionClick(index, position.active)}
-                  d
                 >
                   {position.positionTitle}
                 </div>
