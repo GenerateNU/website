e8c0eaf1df2bd9e03310df81380f547c2da50755
oliviaseds <osedarski@gmail.com>
2024-06-06 18:52:23 -0400

    team card animations

--- src/component/ApplyPageV2/TeamApplicationCard/index.js
+++ src/component/ApplyPageV2/TeamApplicationCard/index.js
@@ -24,7 +24,9 @@ export default function TeamApplicationCard({ team }) {
           {team.team}
         </div>
         <div className='tags-container'>
-          {team.tags ? team.tags.map((tag) => <Tag title={tag}></Tag>) : null}
+          {team.tags
+            ? team.tags.map((tag) => <Tag key={tag} title={tag} />)
+            : null}
         </div>
         {expanded && (
           <div className='paragraph' style={{ color: 'white' }}>
@@ -41,9 +43,10 @@ export default function TeamApplicationCard({ team }) {
             <DownArrow />
             <div className='team-subheader'>{viewRoles}</div>
           </button>
-          {expanded && (
-            <div className='expanded-container'>
-              {team.contributorRoles && team.contributorRoles.length > 0 && (
+          <div className={`expanded-container ${expanded ? 'expanded' : ''}`}>
+            {expanded &&
+              team.contributorRoles &&
+              team.contributorRoles.length > 0 && (
                 <RoleCategory
                   roleCategory={{
                     name: 'Individual Contributors',
@@ -53,28 +56,27 @@ export default function TeamApplicationCard({ team }) {
                   }}
                 />
               )}
-              {team.leadRoles && team.leadRoles.length > 0 && (
-                <RoleCategory
-                  roleCategory={{
-                    name: 'Leaders',
-                    description: team.leadDescription,
-                    roles: team.leadRoles,
-                    color: team.color
-                  }}
-                />
-              )}
-              {team.chiefRoles && team.chiefRoles.length > 0 && (
-                <RoleCategory
-                  roleCategory={{
-                    name: 'Chiefs',
-                    description: team.chiefDescription,
-                    roles: team.chiefRoles,
-                    color: team.color
-                  }}
-                />
-              )}
-            </div>
-          )}
+            {expanded && team.leadRoles && team.leadRoles.length > 0 && (
+              <RoleCategory
+                roleCategory={{
+                  name: 'Leaders',
+                  description: team.leadDescription,
+                  roles: team.leadRoles,
+                  color: team.color
+                }}
+              />
+            )}
+            {expanded && team.chiefRoles && team.chiefRoles.length > 0 && (
+              <RoleCategory
+                roleCategory={{
+                  name: 'Chiefs',
+                  description: team.chiefDescription,
+                  roles: team.chiefRoles,
+                  color: team.color
+                }}
+              />
+            )}
+          </div>
         </div>
       ) : (
         <a href={team.externalLink} target='_blank' rel='noopener noreferrer'>
