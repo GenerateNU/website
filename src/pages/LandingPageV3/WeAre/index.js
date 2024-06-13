1f8a32f309cb539dc693e9dcfa0e0a48359bf564
fraander <57777918+fraander@users.noreply.github.com>
2024-06-13 16:06:39 -0400

    values, kinda

--- src/pages/LandingPageV3/WeAre/index.js
+++ src/pages/LandingPageV3/WeAre/index.js
@@ -6,17 +6,27 @@ export default function WeAre() {
       <div className='blue-left-container' id='we-are'>
         <div className='white-header-text'>We Are...</div>
         <img
+          className='we-are-image'
           src={Placeholder}
-          style={{ width: '480px', marginTop: '1vw' }}
           alt='Placeholder'
         />
         <div id='values'>
-          <div id="values-container">
-            <div id="value-dri" className='value'>Driven</div>
-            <div id="value-emp" className='value'>Empathetic</div>
-            <div id="value-go" className='value'>Growth-oriented</div>
-            <div id="value-inno" className='value'>Innovative</div>
-            <div id="value-spi" className='value'>Sprited</div>
+          <div id='values-container'>
+            <div id='value-dri' className='value'>
+              Driven
+            </div>
+            <div id='value-emp' className='value'>
+              Empathetic
+            </div>
+            <div id='value-go' className='value'>
+              Growth-oriented
+            </div>
+            <div id='value-inno' className='value'>
+              Innovative
+            </div>
+            <div id='value-spi' className='value'>
+              Sprited
+            </div>
           </div>
         </div>
       </div>
