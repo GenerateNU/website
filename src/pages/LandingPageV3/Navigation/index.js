b1690179dc25a9816f76cdc9f88afd816c2705de
fraander <57777918+fraander@users.noreply.github.com>
2024-06-13 15:51:16 -0400

    add social icons (admittedly wrong spot)

--- src/pages/LandingPageV3/Navigation/index.js
+++ src/pages/LandingPageV3/Navigation/index.js
@@ -1,13 +1,34 @@
 import { ReactComponent as GenerateLogo } from '../../../assets/images/landingpage-v3/GenerateLogo.svg'
 import { ReactComponent as DownArrow } from '../../../assets/images/landingpage-v3/DownArrow.svg'
 import './style.css'
+import { ReactComponent as Instagram } from '../../../assets/icons/socials/Instagram.svg'
+import { ReactComponent as Email } from '../../../assets/icons/socials/Email.svg'
+import { ReactComponent as LinkedIn } from '../../../assets/icons/socials/LinkedIn.svg'
 
 export default function Navigation({ scrollToWhatIsGenerate }) {
+  const icons = [
+    {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/generatenu/',
+      symbol: <Instagram className='social-icon' />
+    },
+    {
+      name: 'LinkedIn',
+      href: 'https://www.linkedin.com/company/generate-product-development/mycompany/',
+      symbol: <LinkedIn className='social-icon' />
+    },
+    {
+      name: 'Email',
+      href: 'mailto:generate@northeastern.edu',
+      symbol: <Email className='social-icon' />
+    }
+  ]
+
   return (
-    <div className='bg-row' id="ll1-row">
+    <div className='bg-row' id='ll1-row'>
       <div className='half-container blue-bg'>
         <a href={'/'} className='landing-header'>
-          <GenerateLogo id="generate-logo" />
+          <GenerateLogo id='generate-logo' />
           GENERATE
         </a>
         <div className='landing-links'>
@@ -24,12 +45,26 @@ export default function Navigation({ scrollToWhatIsGenerate }) {
             onClick={scrollToWhatIsGenerate}
             className='landing-nav-link button-link'
           >
-            ABOUT <DownArrow id="about-down-arrow" />
+            ABOUT <DownArrow id='about-down-arrow' />
           </button>
+
+          <div id='socials'>
+            {icons.map((si) => (
+              <a
+                className='social-link'
+                href={si.href}
+                target='_blank'
+                rel='noopener noreferrer'
+                aria-label={si.title}
+              >
+                {si.symbol}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
       <div className='ll-container'>
-        <div id="landing-lines-1" className='landing-lines' />
+        <div id='landing-lines-1' className='landing-lines' />
       </div>
     </div>
   )
