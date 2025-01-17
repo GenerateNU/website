7d64fc42ac901b8e07c031614a4ccceed3692390
fraander <57777918+fraander@users.noreply.github.com>
2025-01-17 11:12:49 -0500

    change dependency array

--- src/services/useSanity.js
+++ src/services/useSanity.js
@@ -4,15 +4,17 @@ import { useState, useEffect } from 'react'
 export function useSanity(query, params = {}, modifier = (data) => data) {
   const [data, setData] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await client.fetch(query, params)
-      const modifiedData = modifier(fetchedData)
-      setData(modifiedData)
-    }
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        const fetchedData = await client.fetch(query, params)
+        const modifiedData = modifier(fetchedData)
+        setData(modifiedData)
+      }
 
-    fetchData()
-  }, [modifier, params, query])
+      fetchData()
+    } /*, [modifier, params, query]*/
+  )
 
   return data
 }
