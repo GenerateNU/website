3f94e71afd69c389f400f3fef9c8ddee7f1d6ec6
Josh Torre <torre.jo@northeastern.edu>
2026-08-02 22:30:33 -0500

    Updated copy and link

--- src/pages/LandingPageV3/index.js
+++ src/pages/LandingPageV3/index.js
@@ -13,6 +13,7 @@ import { Dialog } from '@headlessui/react'
 import { useState } from 'react'
 
 import './style.css'
+import ApplyTodayPopup from '../../component/LandingPage/MemberInfoSession'
 
 export default function LandingPageV3() {
   const whatIsGenerateRef = useRef(null)
@@ -37,6 +38,7 @@ export default function LandingPageV3() {
           tabIndex='0'
         ></iframe>
       </Dialog>
+      <ApplyTodayPopup url='./apply' />
       {isOpen && <div id='background' />}
       <Navigation
         scrollToWhatIsGenerate={() =>
